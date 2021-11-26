fff20c0d66049aca200fa12273a968223b1b562e
Joosep Simm <joosep.simm@guardtime.com>
2021-11-26 13:38:05 +0200

    AB-23 mark math/rand ignored by gosec in test code

--- internal/testutils/random.go
+++ internal/testutils/random.go
@@ -1,13 +1,14 @@
 package test
 
 import (
-	"math/rand" // #nosec 404 - using math/rand, test code
+	"math/rand"
 
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/domain"
 )
 
 func RandomBytes(len int) []byte {
 	bytes := make([]byte, len)
+	// #nosec G404
 	_, err := rand.Read(bytes)
 	if err != nil {
 		panic(err)
@@ -17,8 +18,10 @@ func RandomBytes(len int) []byte {
 
 func RandomPaymentOrder(pt domain.PaymentType) *domain.PaymentOrder {
 	return &domain.PaymentOrder{
-		Type:              pt,
-		BillID:            rand.Uint64(),
+		Type: pt,
+		// #nosec G404
+		BillID: rand.Uint64(),
+		// #nosec G404
 		Amount:            rand.Uint32(),
 		Backlink:          RandomBytes(3),
 		PayeePredicate:    RandomBytes(3),

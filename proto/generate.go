db02c444d731de76a6e07b97120a983fbe0a9e8a
Joosep Simm <joosep.simm@guardtime.com>
2021-12-21 08:44:33 +0200

    AB-43 add transaction protobuf definitions

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,4 @@
 package proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/. --go-grpc_out=paths=source_relative:../internal/rpc/ payment/payment.proto
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ transaction.proto

cddd148dada7fdf0bc880c5882ed4106b24258f2
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-03-14 12:46:08 +0200

    fix typo

--- internal/partition/configuration.go
+++ internal/partition/configuration.go
@@ -7,7 +7,7 @@ type Configuration struct {
 	NodeIdentifier     uint64 // unique within a partition/shard.
 	PartitionNodeCount uint64 // number of nodes in the cluster
 
-	// after a node sees a UC which appoints a leader, the leader waits for t1 time units before sopping accepting new
+	// after a node sees a UC which appoints a leader, the leader waits for t1 time units before stopping accepting new
 	// transaction orders and creating a block proposal.
 	T1Timeout time.Duration
 

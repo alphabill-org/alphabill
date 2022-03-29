36297575f73ab271f34a3bb4702c16f1d46d4bef
Pavel Grigorenko <pavel.grigorenko@guardtime.com>
2022-03-29 17:11:34 +0200

    AB-129 Extracting money tx pb

--- proto/generate.go
+++ proto/generate.go
@@ -2,4 +2,6 @@ package proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ transaction.proto
 
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ moneytx.proto
+
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/alphabill/. --go-grpc_out=paths=source_relative:../internal/rpc/alphabill/ alphabill.proto

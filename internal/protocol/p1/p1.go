7c71b9b6b4f5d44263063906d6d1bb7d4d8f6825
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-04-02 12:55:47 +0300

    AB-109 -  P1 block certification protocol

---
+++ internal/protocol/p1/p1.go
@@ -0,0 +1,89 @@
+package p1
+
+import (
+	"context"
+
+	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/errors"
+	log "gitdc.ee.guardtime.com/alphabill/alphabill/internal/logger"
+	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/network"
+	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/protocol"
+	libp2pNetwork "github.com/libp2p/go-libp2p-core/network"
+)
+
+const ProtocolP1 = "/ab/root/p1/1.0.0"
+
+var ErrPeerIsNil = errors.New("self is nil")
+var ErrRequestHandlerIsNil = errors.New("request handler is nil")
+var logger = log.CreateForPackage()
+
+type P1 struct {
+	self           *network.Peer
+	ctx            context.Context
+	ctxCancel      context.CancelFunc
+	requestHandler chan<- *RequestEvent
+}
+
+type RequestEvent struct {
+	Req        *P1Request
+	ResponseCh chan *P1Response
+}
+
+func New(self *network.Peer, requestHandler chan<- *RequestEvent) (*P1, error) {
+	if self == nil {
+		return nil, ErrPeerIsNil
+	}
+	if requestHandler == nil {
+		return nil, ErrRequestHandlerIsNil
+	}
+	p1 := &P1{
+		self:           self,
+		requestHandler: requestHandler,
+	}
+	p1.ctx, p1.ctxCancel = context.WithCancel(context.Background())
+	self.RegisterProtocolHandler(ProtocolP1, p1.handleStream)
+	return p1, nil
+}
+
+func (p *P1) handleStream(s libp2pNetwork.Stream) {
+	r := protocol.NewProtoBufReader(s)
+	defer r.Close()
+	req := &P1Request{}
+	err := r.Read(req)
+	if err != nil {
+		logger.Warning("Failed to read request: %v", err)
+		return
+	}
+	logger.Debug("Got a new P1 request %v", req)
+	responseChannel := make(chan *P1Response, 1)
+	defer close(responseChannel)
+
+	// send request to requestHandler
+	select {
+	case p.requestHandler <- &RequestEvent{Req: req, ResponseCh: responseChannel}:
+	case <-p.ctx.Done():
+		return
+	}
+
+	var response *P1Response
+	// wait response or cancel
+	select {
+	case response = <-responseChannel:
+	case <-p.ctx.Done():
+		return
+	}
+
+	if response == nil {
+		s.Reset()
+		return
+	}
+	logger.Debug("Got a P1 response %v", response)
+	// write response message
+	writer := protocol.NewProtoBufWriter(s)
+	defer writer.Close()
+	writer.Write(response)
+}
+
+func (p *P1) Close() {
+	p.self.RemoveProtocolHandler(ProtocolP1)
+	p.ctxCancel()
+}
--- proto/generate.go
+++ proto/generate.go
@@ -3,3 +3,5 @@ package proto
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ transaction.proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/alphabill/. --go-grpc_out=paths=source_relative:../internal/rpc/alphabill/ alphabill.proto
+
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/protocol/p1/. p1.proto

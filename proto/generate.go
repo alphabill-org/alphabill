faa190c776ac7913a96eda70a1f022fbedadcfcb
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-04-02 14:02:21 +0300

    move unicity certificate related messages to separate protobuf file

--- proto/generate.go
+++ proto/generate.go
@@ -4,4 +4,6 @@ package proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/alphabill/. --go-grpc_out=paths=source_relative:../internal/rpc/alphabill/ alphabill.proto
 
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/certificates/. certificates.proto
+
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/protocol/p1/. p1.proto

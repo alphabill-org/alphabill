a69bc5d3083d5a984a8efcb1d8b5c53e1f759db6
Pavel Grigorenko <pavel.grigorenko@guardtime.com>
2022-04-08 17:35:21 +0200

    AB-155 VD partition proto

--- internal/rpc/transaction/wrapper_test.go
+++ internal/rpc/transaction/wrapper_test.go
@@ -11,6 +11,8 @@ func TestUint256Hashing(t *testing.T) {
 	// Verifies that the uint256 bytes are equals to the byte array it was made from.
 	// So it doesn't matter if hash is calculated from the byte array from the uint256 byte array.
 	b32 := test.RandomBytes(32)
+	b32[0] = 0x00
+	b32[1] = 0x00
 	b32Int := uint256.NewInt(0).SetBytes(b32)
 	assert.Equal(t, b32, b32Int.Bytes())
 
--- proto/generate.go
+++ proto/generate.go
@@ -4,4 +4,6 @@ package proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ moneytx.proto
 
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ verifiabledatatx.proto
+
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/alphabill/. --go-grpc_out=paths=source_relative:../internal/rpc/alphabill/ alphabill.proto

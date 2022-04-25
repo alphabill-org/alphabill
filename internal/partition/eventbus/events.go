67bd0182e230be6344d42b024d98d19b41fdbb46
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-04-25 10:45:51 +0300

    add default topics

--- internal/partition/eventbus/events.go
+++ internal/partition/eventbus/events.go
@@ -2,6 +2,7 @@ package eventbus
 
 import (
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/certificates"
+	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/protocol/blockproposal"
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/transaction"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
@@ -27,6 +28,15 @@ const (
 	TopicP1 = "root.P1"
 )
 
+var defaultTopics = []string{
+	TopicPartitionUnicityCertificate,
+	TopicPartitionTransaction,
+	TopicBlockProposalInput,
+	TopicBlockProposalOutput,
+	TopicLeaders,
+	TopicP1,
+}
+
 type (
 
 	// UnicityCertificateEvent contains an unicity certificate for the block proposal.
@@ -45,11 +55,7 @@ type (
 
 	// BlockProposalEvent is a block proposal event. See Alphabill yellowpaper for more information.
 	BlockProposalEvent struct {
-		SystemIdentifier   []byte
-		NodeIdentifier     peer.ID
-		UnicityCertificate *certificates.UnicityCertificate
-		Transactions       []*transaction.Transaction
-		// TODO signature
+		*blockproposal.BlockProposal
 	}
 
 	// P1Event is a message sent by the partition node to acquire unicity certificate for the block. See Alphabill

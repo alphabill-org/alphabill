1c959e21afd97b74876ebc373cd483fc6f0393e9
Pavel Grigorenko <pavel.grigorenko@guardtime.com>
2022-05-02 11:17:15 +0200

    removed grpc generation from tx system protos

--- proto/generate.go
+++ proto/generate.go
@@ -2,9 +2,9 @@ package proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/transaction/. transaction.proto
 
-//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ moneytx.proto
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. moneytx.proto
 
-//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. --go-grpc_out=paths=source_relative:../internal/rpc/transaction/ verifiabledatatx.proto
+//go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/transaction/. verifiabledatatx.proto
 
 //go:generate protoc -I=. --go_out=paths=source_relative:../internal/rpc/alphabill/. --go-grpc_out=paths=source_relative:../internal/rpc/alphabill/ alphabill.proto
 

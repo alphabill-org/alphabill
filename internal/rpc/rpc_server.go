091ba5bd6b422434556fa31592e6913fdce1d335
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-05-19 13:35:38 +0300

    refactor rpc server

--- internal/rpc/rpc_server.go
+++ internal/rpc/rpc_server.go
@@ -3,62 +3,37 @@ package rpc
 import (
 	"context"
 
+	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/block"
+
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/errors"
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/errors/errstr"
-	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/partition"
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/rpc/alphabill"
 	"gitdc.ee.guardtime.com/alphabill/alphabill/internal/txsystem"
 )
 
 type (
-	// Deprecated. Will be removed.
 	rpcServer struct {
 		alphabill.UnimplementedAlphabillServiceServer
-		processor     TransactionsProcessor
-		ledgerService LedgerService
-	}
-	// TODO rename to rpcServer
-	rpcServer2 struct {
-		alphabill.UnimplementedAlphabillServiceServer
-		node *partition.Node
+		node partitionNode
 	}
 
-	// TODO remove
-	TransactionsProcessor interface {
-		Convert(tx *txsystem.Transaction) (txsystem.GenericTransaction, error)
-		Process(gtx txsystem.GenericTransaction) error
-	}
-
-	// TODO remove
-	LedgerService interface {
-		GetBlock(request *alphabill.GetBlockRequest) (*alphabill.GetBlockResponse, error)
-		GetMaxBlockNo(request *alphabill.GetMaxBlockNoRequest) (*alphabill.GetMaxBlockNoResponse, error)
+	partitionNode interface {
+		SubmitTx(tx *txsystem.Transaction) error
+		GetBlock(blockNr uint64) (*block.Block, error)
+		GetLatestBlock() *block.Block
 	}
 )
 
-// Deprecated. Will be removed.
-func NewRpcServer(processor TransactionsProcessor, ledgerService LedgerService) (*rpcServer, error) {
-	if processor == nil || ledgerService == nil {
-		return nil, errors.Wrap(errors.ErrInvalidArgument, errstr.NilArgument)
-	}
-	return &rpcServer{
-		processor:     processor,
-		ledgerService: ledgerService,
-	}, nil
-}
-
-// TODO rename to NewRpcServer
-func NewRpcServer2(node *partition.Node) (*rpcServer2, error) {
-	//if node == nil || ledgerService == nil {
+func NewRpcServer(node partitionNode) (*rpcServer, error) {
 	if node == nil {
 		return nil, errors.Wrap(errors.ErrInvalidArgument, errstr.NilArgument)
 	}
-	return &rpcServer2{
+	return &rpcServer{
 		node: node,
 	}, nil
 }
 
-func (r *rpcServer2) ProcessTransaction(ctx context.Context, tx *txsystem.Transaction) (*txsystem.TransactionResponse, error) {
+func (r *rpcServer) ProcessTransaction(ctx context.Context, tx *txsystem.Transaction) (*txsystem.TransactionResponse, error) {
 	err := r.node.SubmitTx(tx)
 	if err != nil {
 		return &txsystem.TransactionResponse{
@@ -72,7 +47,7 @@ func (r *rpcServer2) ProcessTransaction(ctx context.Context, tx *txsystem.Transa
 	}, nil
 }
 
-func (r *rpcServer2) GetBlock(ctx context.Context, req *alphabill.GetBlockRequest) (*alphabill.GetBlockResponse, error) {
+func (r *rpcServer) GetBlock(ctx context.Context, req *alphabill.GetBlockRequest) (*alphabill.GetBlockResponse, error) {
 	block, err := r.node.GetBlock(req.BlockNo)
 	if err != nil {
 		return &alphabill.GetBlockResponse{ErrorMessage: err.Error()}, err
@@ -80,44 +55,7 @@ func (r *rpcServer2) GetBlock(ctx context.Context, req *alphabill.GetBlockReques
 	return &alphabill.GetBlockResponse{Block: block}, nil
 }
 
-func (r *rpcServer2) GetMaxBlockNo(ctx context.Context, req *alphabill.GetMaxBlockNoRequest) (*alphabill.GetMaxBlockNoResponse, error) {
+func (r *rpcServer) GetMaxBlockNo(ctx context.Context, req *alphabill.GetMaxBlockNoRequest) (*alphabill.GetMaxBlockNoResponse, error) {
 	maxBlockNr := r.node.GetLatestBlock().GetBlockNumber()
 	return &alphabill.GetMaxBlockNoResponse{BlockNo: maxBlockNr}, nil
-}
-
-func (r *rpcServer) ProcessTransaction(ctx context.Context, tx *txsystem.Transaction) (*txsystem.TransactionResponse, error) {
-	genTx, err := r.processor.Convert(tx)
-	if err != nil {
-		return &txsystem.TransactionResponse{
-			Ok:      false,
-			Message: err.Error(),
-		}, err
-	}
-	err = r.processor.Process(genTx)
-	if err != nil {
-		return &txsystem.TransactionResponse{
-			Ok:      false,
-			Message: err.Error(),
-		}, err
-	}
-	return &txsystem.TransactionResponse{
-		Ok:      true,
-		Message: "",
-	}, nil
-}
-
-func (r *rpcServer) GetBlock(ctx context.Context, req *alphabill.GetBlockRequest) (*alphabill.GetBlockResponse, error) {
-	block, err := r.ledgerService.GetBlock(req)
-	if err != nil {
-		return &alphabill.GetBlockResponse{ErrorMessage: err.Error()}, err
-	}
-	return block, nil
-}
-
-func (r *rpcServer) GetMaxBlockNo(ctx context.Context, req *alphabill.GetMaxBlockNoRequest) (*alphabill.GetMaxBlockNoResponse, error) {
-	maxBlockNo, err := r.ledgerService.GetMaxBlockNo(req)
-	if err != nil {
-		return &alphabill.GetMaxBlockNoResponse{ErrorMessage: err.Error()}, err
-	}
-	return maxBlockNo, nil
 }

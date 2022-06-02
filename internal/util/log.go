36f885b375619ac4956664288d7ff65737b613d2
Pavel Grigorenko <pavel.grigorenko@guardtime.com>
2022-06-02 14:28:50 +0200

    AB-265 Fixed WriteDebugJsonLog

--- internal/util/log.go
+++ internal/util/log.go
@@ -7,7 +7,7 @@ import (
 )
 
 func WriteDebugJsonLog(l log.Logger, m string, arg interface{}) {
-	if l.GetLevel() == log.DEBUG {
+	if l.GetLevel() >= log.DEBUG {
 		j, _ := json.MarshalIndent(arg, "", "\t")
 		l.Debug("%s\n%s", m, string(j))
 	}

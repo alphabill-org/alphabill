03f58e4ab9d3376f5454a8cca01577ffcc3bf71c
Pavel Grigorenko <pgrigorenko@gmail.com>
2022-07-15 15:24:51 +0200

    made start/resume/stop scripts compatible. e.g. it is now possible first to run start-all, then do stop-money and then resume-money

--- resume-vd.sh
+++ resume-vd.sh
@@ -6,7 +6,7 @@ port=26666
 # partition node addresses
 for i in 1 2 3
 do
-  id=$(build/alphabill identifier -k testab/node$i/vd/keys.json | tail -n1)
+  id=$(build/alphabill identifier -k testab/vd$i/vd/keys.json | tail -n1)
   nodeAddresses="$nodeAddresses,$id=/ip4/127.0.0.1/tcp/$port";
 
   ((port=port+1))
@@ -19,7 +19,7 @@ grpcPort=26766
 #start partition nodes
 for i in 1 2 3
 do
-  build/alphabill vd --home testab/node$i -f testab/node$i/vd/blocks.db -k testab/node$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/partition-genesis-1.json -p "$nodeAddresses" > "testab/node$i/log.log" &
+  build/alphabill vd --home testab/vd$i -f testab/vd$i/vd/blocks.db -k testab/vd$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/partition-genesis-1.json -p "$nodeAddresses" > "testab/vd$i/log.log" &
   ((port=port+1))
   ((grpcPort=grpcPort+1))
 done
--- start-vd.sh
+++ start-vd.sh
@@ -8,22 +8,22 @@ nodeAddresses=""
 # Generate node genesis files.
 for i in 1 2 3
 do
-  mkdir testab/node$i
+  mkdir testab/vd$i
   # "-g" flags also generates keys
-  build/alphabill vd-genesis --home testab/node$i -g
+  build/alphabill vd-genesis --home testab/vd$i -g
 done
 
 # generate rootchain and partition genesis files
-build/alphabill root-genesis --home testab -p testab/node1/vd/node-genesis.json -p testab/node2/vd/node-genesis.json -p testab/node3/vd/node-genesis.json -k testab/rootchain/keys.json -g
+build/alphabill root-genesis --home testab -p testab/vd1/vd/node-genesis.json -p testab/vd2/vd/node-genesis.json -p testab/vd3/vd/node-genesis.json -k testab/rootchain/keys.json -g
 
 #start root chain
 build/alphabill root --home testab -k testab/rootchain/keys.json -g testab/rootchain/root-genesis.json > testab/rootchain/log.log &
 
-port=26666
+port=27666
 # partition node addresses
 for i in 1 2 3
 do
-  id=$(build/alphabill identifier -k testab/node$i/vd/keys.json | tail -n1)
+  id=$(build/alphabill identifier -k testab/vd$i/vd/keys.json | tail -n1)
   nodeAddresses="$nodeAddresses,$id=/ip4/127.0.0.1/tcp/$port";
 
   ((port=port+1))
@@ -31,12 +31,12 @@ done
 
 nodeAddresses="${nodeAddresses:1}"
 
-port=26666
-grpcPort=26766
+port=27666
+grpcPort=27766
 #start partition nodes
 for i in 1 2 3
 do
-  build/alphabill vd --home testab/node$i -f testab/node$i/vd/blocks.db -k testab/node$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/partition-genesis-1.json -p "$nodeAddresses" > "testab/node$i/log.log" &
+  build/alphabill vd --home testab/vd$i -f testab/vd$i/vd/blocks.db -k testab/vd$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/partition-genesis-1.json -p "$nodeAddresses" > "testab/vd$i/log.log" &
   ((port=port+1))
   ((grpcPort=grpcPort+1))
 done

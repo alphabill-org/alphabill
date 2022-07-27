b22bcabc0b3e3f430dd28654724e2ed2a125fb16
Pavel Grigorenko <pgrigorenko@gmail.com>
2022-07-27 13:30:46 +0200

    append to logs upon resuming nodes

--- resume-money.sh
+++ resume-money.sh
@@ -17,7 +17,7 @@ grpcPort=26766
 #start partition nodes
 for i in 1 2 3
 do
-  build/alphabill money --home testab/money$i -f testab/money$i/money/blocks.db -k testab/money$i/money/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/rootchain/partition-genesis-0.json -p "$nodeAddresses" > "testab/money$i/log.log" &
+  build/alphabill money --home testab/money$i -f testab/money$i/money/blocks.db -k testab/money$i/money/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/rootchain/partition-genesis-0.json -p "$nodeAddresses" >> "testab/money$i/log.log" &
   ((port=port+1))
   ((grpcPort=grpcPort+1))
 done

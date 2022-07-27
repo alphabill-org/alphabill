901e29d8d940bf7de69f8ff310c62135709c35a7
RaulMaesalu <raul.maesalu@guardtime.com>
2022-07-27 17:09:07 +0300

    AB-306 changed root genesis output directory

--- resume-vd.sh
+++ resume-vd.sh
@@ -19,7 +19,7 @@ grpcPort=27766
 #start partition nodes
 for i in 1 2 3
 do
-  build/alphabill vd --home testab/vd$i -f testab/vd$i/vd/blocks.db -k testab/vd$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/rootchain/partition-genesis-1.json -p "$nodeAddresses" > "testab/vd$i/log.log" &
+  build/alphabill vd --home testab/vd$i -f testab/vd$i/vd/blocks.db -k testab/vd$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$port" --server-address ":$grpcPort" -g testab/rootchain/genesis/partition-genesis-1.json -p "$nodeAddresses" > "testab/vd$i/log.log" &
   ((port=port+1))
   ((grpcPort=grpcPort+1))
 done

d6e68c86cb8ea64a219e612ed1f37f7b7529d43a
Rando Mihkelsaar <rando.mihkelsaar@guardtime.com>
2022-10-07 10:52:58 +0300

    improve shell scripts to support User Token Partition

--- start-all.sh
+++ start-all.sh
@@ -5,6 +5,7 @@ mkdir testab
 mkdir testab/rootchain
 moneyNodeAddresses=""
 vdNodeAddresses=""
+tokensNodeAddresses=""
 
 # Generate money node genesis files.
 for i in 1 2 3
@@ -21,8 +22,16 @@ do
   build/alphabill vd-genesis --home testab/vd$i -g
 done
 
+# Generate token partition node genesis files.
+for i in 1 2 3
+do
+  mkdir testab/tokens$i
+  # "-g" flags also generates keys
+  build/alphabill tokens-genesis --home testab/tokens$i -g
+done
+
 # generate rootchain and partition genesis files
-build/alphabill root-genesis --home testab/rootchain -o testab/rootchain/genesis -p testab/money1/money/node-genesis.json -p testab/money2/money/node-genesis.json -p testab/money3/money/node-genesis.json -p testab/vd1/vd/node-genesis.json -p testab/vd2/vd/node-genesis.json -p testab/vd3/vd/node-genesis.json -k testab/rootchain/keys.json -g
+build/alphabill root-genesis --home testab/rootchain -o testab/rootchain/genesis -p testab/tokens1/tokens/node-genesis.json -p testab/tokens2/tokens/node-genesis.json -p testab/tokens3/tokens/node-genesis.json -p testab/money1/money/node-genesis.json -p testab/money2/money/node-genesis.json -p testab/money3/money/node-genesis.json -p testab/vd1/vd/node-genesis.json -p testab/vd2/vd/node-genesis.json -p testab/vd3/vd/node-genesis.json -k testab/rootchain/keys.json -g
 
 #start root chain
 build/alphabill root --home testab/rootchain -f testab/rootchain/rounds.db -k testab/rootchain/keys.json -g testab/rootchain/genesis/root-genesis.json > testab/rootchain/rootchain.log &
@@ -69,4 +78,26 @@ do
   build/alphabill vd --home testab/vd$i -f testab/vd$i/vd/blocks.db -k testab/vd$i/vd/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$vdPort" --server-address ":$vdGrpcPort" -g testab/rootchain/genesis/partition-genesis-1.json -p "$vdNodeAddresses" > "testab/vd$i/vd$i.log" &
   ((vdPort=vdPort+1))
   ((vdGrpcPort=vdGrpcPort+1))
+done
+
+tokensPort=28666
+# tokens partition node addresses
+for i in 1 2 3
+do
+  id=$(build/alphabill identifier -k testab/tokens$i/tokens/keys.json | tail -n1)
+  tokensNodeAddresses="$tokensNodeAddresses,$id=/ip4/127.0.0.1/tcp/$tokensPort";
+
+  ((tokensPort=tokensPort+1))
+done
+
+tokensNodeAddresses="${tokensNodeAddresses:1}"
+
+tokensPort=28666
+tokensGrpcPort=28866
+#start tokens partition nodes
+for i in 1 2 3
+do
+  build/alphabill tokens --home testab/tokens$i -f testab/tokens$i/tokens/blocks.db -k testab/tokens$i/tokens/keys.json -r "/ip4/127.0.0.1/tcp/26662" -a "/ip4/127.0.0.1/tcp/$tokensPort" --server-address ":$tokensGrpcPort" -g testab/rootchain/genesis/partition-genesis-2.json -p "$tokensNodeAddresses" > "testab/tokens$i/tokens$i.log" &
+  ((tokensPort=tokensPort+1))
+  ((tokensGrpcPort=tokensGrpcPort+1))
 done

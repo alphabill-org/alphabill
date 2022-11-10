bc6d75b98f503af7be0b4d546bef7e578ff971fc
Pavel Grigorenko <pgrigorenko@gmail.com>
2022-11-10 11:22:16 +0100

    improved scripts

--- test/scripts/listTokens.sh
+++ test/scripts/listTokens.sh
@@ -15,8 +15,7 @@ echo "List token types"
 build/alphabill wallet token list-types -u localhost:27766 --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/
 
 echo "List the tokens w1"
-build/alphabill wallet token list fungible --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/ -u localhost:27766
+build/alphabill wallet token list --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/ -u localhost:27766 -k -1
 
 echo "List the tokens w2"
-build/alphabill wallet get-pubkeys --log-level DEBUG --log-file $abHome/wallet2/w2.log -l $abHome/wallet2/
-build/alphabill wallet token list fungible --log-level DEBUG --log-file $abHome/wallet2/w2.log -l $abHome/wallet2/ -u localhost:27766
+build/alphabill wallet token list --log-level DEBUG --log-file $abHome/wallet2/w2.log -l $abHome/wallet2/ -u localhost:27766 -k -1
--- test/scripts/mintToken.sh
+++ test/scripts/mintToken.sh
@@ -9,15 +9,15 @@ echo "abBin=$abBin"
 
 cd $abBin
 
-echo "List token types"
+echo "List token types w1"
 build/alphabill wallet token list-types -u localhost:27766 --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/
 
-echo "Mint new token"
+echo "Mint new token w1"
 tokenTypeId=1000000000000000000000000000000000000000000000000000000000000000
 build/alphabill wallet token new fungible --type $tokenTypeId --amount 1 --log-level DEBUG -l ~/.alphabill/wallet1/ --log-file $abHome/wallet1/w1.log -u localhost:27766
 build/alphabill wallet token new fungible --type $tokenTypeId --amount 2 --log-level DEBUG -l ~/.alphabill/wallet1/ --log-file $abHome/wallet1/w1.log -u localhost:27766
 build/alphabill wallet token new fungible --type $tokenTypeId --amount 5 --log-level DEBUG -l ~/.alphabill/wallet1/ --log-file $abHome/wallet1/w1.log -u localhost:27766
 
-echo "List the tokens"
-build/alphabill wallet token list fungible --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/ -u localhost:27766
+echo "List the tokens w1"
+build/alphabill wallet token list --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/ -u localhost:27766
 

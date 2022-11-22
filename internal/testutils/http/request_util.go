d60cd05926b875e5d5f0cc3a99046053810dc6d8
Lennart Ploom <lennart.ploom@guardtime.com>
2022-11-22 13:30:55 +0200

    fix sonar duplicate code warnings

--- internal/testutils/http/request_util.go
+++ internal/testutils/http/request_util.go
@@ -14,57 +14,57 @@ import (
 )
 
 func DoGet(t *testing.T, url string, response interface{}) *http.Response {
-	httpRes, err := http.Get(url) // #nosec G107
+	httpRes, resBytes, err := doGet(url)
 	if err != nil {
-		fmt.Println(err)
 		return nil
 	}
-	defer func() {
-		_ = httpRes.Body.Close()
-	}()
-	resBytes, _ := ioutil.ReadAll(httpRes.Body)
-	fmt.Printf("GET %s response: %s\n", url, string(resBytes))
 	err = json.NewDecoder(bytes.NewReader(resBytes)).Decode(response)
 	require.NoError(t, err)
 	return httpRes
 }
 
-func DoPost(t *testing.T, url string, req interface{}, res interface{}) *http.Response {
-	reqBodyBytes, err := json.Marshal(req)
-	require.NoError(t, err)
-	httpRes, err := http.Post(url, "application/json", bytes.NewBuffer(reqBodyBytes)) // #nosec G107
-	require.NoError(t, err)
-	defer func() {
-		_ = httpRes.Body.Close()
-	}()
-	resBytes, _ := ioutil.ReadAll(httpRes.Body)
-	fmt.Printf("POST %s response: %s\n", url, string(resBytes))
-	err = json.NewDecoder(bytes.NewReader(resBytes)).Decode(res)
+func DoGetProto(t *testing.T, url string, response proto.Message) *http.Response {
+	httpRes, resBytes, err := doGet(url)
+	if err != nil {
+		return nil
+	}
+	err = protojson.Unmarshal(resBytes, response)
 	require.NoError(t, err)
 	return httpRes
 }
 
-func DoGetProto(t *testing.T, url string, response proto.Message) *http.Response {
+func DoPost(t *testing.T, url string, req interface{}, res interface{}) *http.Response {
+	reqBody, err := json.Marshal(req)
+	require.NoError(t, err)
+	return doPost(t, url, reqBody, res)
+}
+
+func DoPostProto(t *testing.T, url string, req proto.Message, res interface{}) *http.Response {
+	reqBody, err := protojson.Marshal(req)
+	require.NoError(t, err)
+	return doPost(t, url, reqBody, res)
+}
+
+func doGet(url string) (*http.Response, []byte, error) {
 	httpRes, err := http.Get(url) // #nosec G107
 	if err != nil {
 		fmt.Println(err)
-		return nil
+		return nil, nil, err
 	}
 	defer func() {
 		_ = httpRes.Body.Close()
 	}()
 	resBytes, _ := ioutil.ReadAll(httpRes.Body)
 	fmt.Printf("GET %s response: %s\n", url, string(resBytes))
-	err = protojson.Unmarshal(resBytes, response)
-	require.NoError(t, err)
-	return httpRes
+	return httpRes, resBytes, nil
 }
 
-func DoPostProto(t *testing.T, url string, req proto.Message, res interface{}) *http.Response {
-	reqBodyBytes, err := protojson.Marshal(req)
-	require.NoError(t, err)
-	httpRes, err := http.Post(url, "application/json", bytes.NewBuffer(reqBodyBytes)) // #nosec G107
-	require.NoError(t, err)
+func doPost(t *testing.T, url string, reqBody []byte, res interface{}) *http.Response {
+	httpRes, err := http.Post(url, "application/json", bytes.NewBuffer(reqBody)) // #nosec G107
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 	defer func() {
 		_ = httpRes.Body.Close()
 	}()

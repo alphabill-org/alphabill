52f58cab22fa543173429a4a7b29c5a90fbfa266
Pavel Grigorenko <pgrigorenko@gmail.com>
2022-11-23 16:30:11 +0100

    TestTokensProcessBlock_withTx_sendTokens

--- pkg/wallet/tokens/token_blocks_test.go
+++ pkg/wallet/tokens/token_blocks_test.go
@@ -2,12 +2,14 @@ package tokens
 
 import (
 	"context"
+	"crypto"
 	"testing"
 
 	"github.com/alphabill-org/alphabill/internal/block"
 	"github.com/alphabill-org/alphabill/internal/script"
 	"github.com/alphabill-org/alphabill/internal/txsystem"
 	"github.com/alphabill-org/alphabill/internal/txsystem/tokens"
+	txutil "github.com/alphabill-org/alphabill/internal/txsystem/util"
 	"github.com/alphabill-org/alphabill/internal/util"
 	"github.com/holiman/uint256"
 	"github.com/stretchr/testify/require"
@@ -125,6 +127,119 @@ func TestTokensProcessBlock_withTx_mintTokens(t *testing.T) {
 	token2, err := w.db.Do().GetToken(1, util.Uint256ToBytes(uint256.NewInt(0).SetBytes(id2)))
 	require.NoError(t, err)
 	require.Equal(t, TokenTypeID(typeId2), token2.GetTypeId())
+}
+
+func TestTokensProcessBlock_withTx_sendTokens(t *testing.T) {
+	w, client := createTestWallet(t)
+	w.sync = true
+	defer w.Shutdown()
+
+	acc2idx, pubKey2, err := w.mw.AddAccount()
+	acc2 := acc2idx + 1
+	require.NoError(t, err)
+	// prepare tokens on account 1
+	typeID1 := util.Uint256ToBytes(uint256.NewInt(0).SetBytes([]byte{0x10}))
+	id1 := util.Uint256ToBytes(uint256.NewInt(0).SetBytes([]byte{0x11}))
+	id2 := util.Uint256ToBytes(uint256.NewInt(0).SetBytes([]byte{0x12}))
+	typeID2 := util.Uint256ToBytes(uint256.NewInt(0).SetBytes([]byte{0x20}))
+	nft1 := util.Uint256ToBytes(uint256.NewInt(0).SetBytes([]byte{0x21}))
+
+	require.NoError(t, w.db.Do().SetToken(1, &TokenUnit{
+		TypeID: typeID1,
+		ID:     id1,
+		Symbol: "AB",
+		Amount: 1,
+		Kind:   FungibleToken,
+	}))
+	require.NoError(t, w.db.Do().SetToken(1, &TokenUnit{
+		TypeID: typeID1,
+		ID:     id2,
+		Symbol: "AB",
+		Amount: 5,
+		Kind:   FungibleToken,
+	}))
+	require.NoError(t, w.db.Do().SetToken(1, &TokenUnit{
+		TypeID: typeID2,
+		ID:     nft1,
+		Symbol: "ABNFT",
+		Kind:   NonFungibleToken,
+	}))
+
+	// check db state
+	tokens1, err := w.db.Do().GetTokens(1)
+	require.NoError(t, err)
+	require.Len(t, tokens1, 3)
+	tokens2, err := w.db.Do().GetTokens(acc2)
+	require.NoError(t, err)
+	require.Len(t, tokens2, 0)
+
+	//prepare block
+	blockNr := uint64(1)
+	// fungible transfer
+	tx1 := createTx(id1, blockNr)
+	require.NoError(t, err)
+	require.NoError(t, anypb.MarshalFrom(tx1.TransactionAttributes, &tokens.TransferFungibleTokenAttributes{
+		NewBearer: bearerPredicateFromPubKey(pubKey2),
+		Value:     uint64(1),
+	}, proto.MarshalOptions{}))
+
+	// fungible split
+	tx2 := createTx(id2, blockNr)
+	require.NoError(t, err)
+	require.NoError(t, anypb.MarshalFrom(tx2.TransactionAttributes, &tokens.SplitFungibleTokenAttributes{
+		NewBearer:   bearerPredicateFromPubKey(pubKey2),
+		TargetValue: 2,
+	}, proto.MarshalOptions{}))
+	// transfer nft
+	nftTx := createTx(nft1, blockNr)
+	require.NoError(t, anypb.MarshalFrom(nftTx.TransactionAttributes, &tokens.TransferNonFungibleTokenAttributes{
+		NewBearer: bearerPredicateFromPubKey(pubKey2),
+	}, proto.MarshalOptions{}))
+	//build block
+	client.SetMaxBlockNumber(blockNr)
+	client.SetBlock(&block.Block{
+		SystemIdentifier: tokens.DefaultTokenTxSystemIdentifier,
+		BlockNumber:      blockNr,
+		Transactions:     []*txsystem.Transaction{tx1, tx2, nftTx}})
+
+	require.NoError(t, w.Sync(context.Background()))
+
+	blockNrFromDB, err := w.db.Do().GetBlockNumber()
+	require.NoError(t, err)
+	require.Equal(t, blockNr, blockNrFromDB)
+
+	tokens1, err = w.db.Do().GetTokens(1)
+	require.NoError(t, err)
+	// a portion of token2 remains after split
+	require.Len(t, tokens1, 1)
+	tokens2, err = w.db.Do().GetTokens(acc2)
+	require.NoError(t, err)
+	require.Len(t, tokens2, 3)
+
+	token1, err := w.db.Do().GetToken(acc2, id1)
+	require.NoError(t, err)
+	//require.Equal(t, TokenID(id1), token1.GetTypeId()) // TODO type info is missing
+	require.Equal(t, FungibleToken, token1.Kind)
+	require.Equal(t, uint64(1), token1.Amount)
+
+	//split token
+	token2, err := w.db.Do().GetToken(1, id2)
+	require.NoError(t, err)
+	require.Equal(t, uint64(3), token2.Amount) // 3-2=1
+
+	gtx, err := w.txs.ConvertTx(tx2)
+	require.NoError(t, err)
+	newId2 := txutil.SameShardIDBytes(uint256.NewInt(0).SetBytes(id2), gtx.(tokens.SplitFungibleToken).HashForIDCalculation(crypto.SHA256))
+	splitToken2, err := w.db.Do().GetToken(acc2, newId2)
+	require.NoError(t, err)
+	//require.Equal(t, TokenID(id2), token2.GetTypeId()) // TODO type info is missing
+	require.Equal(t, FungibleToken, splitToken2.Kind)
+	require.Equal(t, uint64(2), splitToken2.Amount)
+	//nft
+	token3, err := w.db.Do().GetToken(acc2, nft1)
+	require.NoError(t, err)
+	//require.Equal(t, TokenID(nft1), token3.GetTypeId()) // TODO type info is missing
+	require.Equal(t, NonFungibleToken, token3.Kind)
 }
 
 func TestTokensProcessBlock_syncToUnit(t *testing.T) {

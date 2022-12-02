6cc3841bafc0e6d050aff542f412814ac82c86ab
Pavel Grigorenko <pgrigorenko@gmail.com>
2022-12-02 11:45:40 +0100

    updated test script

--- test/scripts/resetTokens.sh
+++ test/scripts/resetTokens.sh
@@ -34,7 +34,7 @@ echo "mainKey2=$mainKey2"
 echo "Create new fungible token type"
 typeId1=1000000000000000000000000000000000000000000000000000000000000000
 
-build/alphabill wallet token new-type fungible --symbol AB --type $typeId1 -u localhost:27766 --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/
+build/alphabill wallet token new-type fungible --decimals 0 --symbol AB --type $typeId1 -u localhost:27766 --log-level DEBUG --log-file $abHome/wallet1/w1.log -l $abHome/wallet1/
 
 echo "Create new NFT type"
 typeId2=2000000000000000000000000000000000000000000000000000000000000000

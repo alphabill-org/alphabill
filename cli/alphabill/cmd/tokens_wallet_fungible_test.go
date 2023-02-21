450c90a1d2659d73faee931d04544fea6c2d42eb
Pavel Grigorenko <pgrigorenko@gmail.com>
2023-02-21 18:19:15 +0100

    refactoring cmd token wallet tests

--- cli/alphabill/cmd/tokens_wallet_fungible_test.go
+++ cli/alphabill/cmd/tokens_wallet_fungible_test.go
@@ -15,41 +15,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestWalletCreateFungibleTokenTypeCmd_SymbolFlag(t *testing.T) {
-	homedir := createNewTestWallet(t)
-	// missing symbol parameter
-	_, err := execCommand(homedir, "token new-type fungible --decimals 3")
-	require.ErrorContains(t, err, "required flag(s) \"symbol\" not set")
-	// symbol parameter not set
-	_, err = execCommand(homedir, "flag needs an argument: --symbol")
-	// there currently are no restrictions on symbol length on CLI side
-}
-
-func TestWalletCreateFungibleTokenTypeCmd_TypeIdlFlag(t *testing.T) {
-	homedir := createNewTestWallet(t)
-	// hidden parameter type (not a mandatory parameter)
-	_, err := execCommand(homedir, "token new-type fungible --symbol \"@1\" --type")
-	require.ErrorContains(t, err, "flag needs an argument: --type")
-	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --type 011")
-	require.ErrorContains(t, err, "invalid argument \"011\" for \"--type\" flag: encoding/hex: odd length hex string")
-	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --type foo")
-	require.ErrorContains(t, err, "invalid argument \"foo\" for \"--type\" flag")
-	// there currently are no restrictions on type length on CLI side
-}
-
-func TestWalletCreateFungibleTokenTypeCmd_DecimalsFlag(t *testing.T) {
-	homedir := createNewTestWallet(t)
-	// hidden parameter type (not a mandatory parameter)
-	_, err := execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals")
-	require.ErrorContains(t, err, "flag needs an argument: --decimals")
-	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals foo")
-	require.ErrorContains(t, err, "invalid argument \"foo\" for \"--decimals\" flag")
-	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals -1")
-	require.ErrorContains(t, err, "invalid argument \"-1\" for \"--decimals\"")
-	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals 9")
-	require.ErrorContains(t, err, "argument \"9\" for \"--decimals\" flag is out of range, max value 8")
-}
-
 func TestFungibleToken_Subtyping_Integration(t *testing.T) {
 	partition := startTokensPartition(t)
 
@@ -205,22 +170,6 @@ func TestFungibleTokens_Sending_Integration(t *testing.T) {
 	verifyStdoutEventually(t, func() *testConsoleWriter {
 		return execTokensCmd(t, homedirW1, fmt.Sprintf("list fungible -u %s", backendUrl))
 	}, "amount='2'", "amount='6'")
-}
-
-func TestWalletCreateFungibleTokenCmd_TypeFlag(t *testing.T) {
-	homedir := createNewTestWallet(t)
-	_, err := execCommand(homedir, "token new fungible --type A8B")
-	require.ErrorContains(t, err, "invalid argument \"A8B\" for \"--type\" flag: encoding/hex: odd length hex string")
-	_, err = execCommand(homedir, "token new fungible --type nothex")
-	require.ErrorContains(t, err, "invalid argument \"nothex\" for \"--type\" flag: encoding/hex: invalid byte")
-	_, err = execCommand(homedir, "token new fungible --amount 4")
-	require.ErrorContains(t, err, "required flag(s) \"type\" not set")
-}
-
-func TestWalletCreateFungibleTokenCmd_AmountFlag(t *testing.T) {
-	homedir := createNewTestWallet(t)
-	_, err := execCommand(homedir, "token new fungible --type A8BB")
-	require.ErrorContains(t, err, "required flag(s) \"amount\" not set")
 }
 
 func TestWalletCreateFungibleTokenTypeAndTokenAndSendCmd_IntegrationTest(t *testing.T) {
--- cli/alphabill/cmd/tokens_wallet_test.go
+++ cli/alphabill/cmd/tokens_wallet_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	gocrypto "crypto"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"path/filepath"
 	"strings"
@@ -21,59 +22,9 @@ import (
 	tw "github.com/alphabill-org/alphabill/pkg/wallet/tokens"
 	twb "github.com/alphabill-org/alphabill/pkg/wallet/tokens/backend"
 	"github.com/alphabill-org/alphabill/pkg/wallet/tokens/client"
-	"github.com/holiman/uint256"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 )
-
-type accountManagerMock struct {
-	keyHash       []byte
-	recordedIndex uint64
-}
-
-func (a *accountManagerMock) GetAccountKey(accountIndex uint64) (*account.AccountKey, error) {
-	a.recordedIndex = accountIndex
-	return &account.AccountKey{PubKeyHash: &account.KeyHashes{Sha256: a.keyHash}}, nil
-}
-
-func (a *accountManagerMock) GetAll() []account.Account {
-	return nil
-}
-
-func (a *accountManagerMock) CreateKeys(mnemonic string) error {
-	return nil
-}
-
-func (a *accountManagerMock) AddAccount() (uint64, []byte, error) {
-	return 0, nil, nil
-}
-
-func (a *accountManagerMock) GetMnemonic() (string, error) {
-	return "", nil
-}
-
-func (a *accountManagerMock) GetAccountKeys() ([]*account.AccountKey, error) {
-	return nil, nil
-}
-
-func (a *accountManagerMock) GetMaxAccountIndex() (uint64, error) {
-	return 0, nil
-}
-
-func (a *accountManagerMock) GetPublicKey(accountIndex uint64) ([]byte, error) {
-	return nil, nil
-}
-
-func (a *accountManagerMock) GetPublicKeys() ([][]byte, error) {
-	return nil, nil
-}
-
-func (a *accountManagerMock) IsEncrypted() (bool, error) {
-	return false, nil
-}
-
-func (a *accountManagerMock) Close() {
-}
 
 func TestParsePredicateClause(t *testing.T) {
 	mock := &accountManagerMock{keyHash: []byte{0x1, 0x2}}
@@ -566,20 +517,264 @@ func TestListTokensCommandInputs(t *testing.T) {
 	}
 }
 
-func ensureUnitBytes(t *testing.T, state tokens.TokenState, id []byte) *rma.Unit {
-	return ensureUnit(t, state, uint256.NewInt(0).SetBytes(id))
+func TestListTokensTypesCommandInputs(t *testing.T) {
+	tests := []struct {
+		name         string
+		args         []string
+		expectedKind twb.Kind
+		expectedPass string
+	}{
+		{
+			name:         "list all tokens",
+			args:         []string{},
+			expectedKind: twb.Any,
+		},
+		{
+			name:         "list all tokens, encrypted wallet",
+			args:         []string{"--pn", "test pass phrase"},
+			expectedKind: twb.Any,
+			expectedPass: "test pass phrase",
+		},
+		{
+			name:         "list all fungible tokens",
+			args:         []string{"fungible"},
+			expectedKind: twb.Fungible,
+		},
+		{
+			name:         "list all fungible tokens, encrypted wallet",
+			args:         []string{"fungible", "--pn", "test pass phrase"},
+			expectedKind: twb.Fungible,
+			expectedPass: "test pass phrase",
+		},
+		{
+			name:         "list all non-fungible tokens",
+			args:         []string{"non-fungible"},
+			expectedKind: twb.NonFungible,
+		},
+		{
+			name:         "list all non-fungible tokens, encrypted wallet",
+			args:         []string{"non-fungible", "--pn", "test pass phrase"},
+			expectedKind: twb.NonFungible,
+			expectedPass: "test pass phrase",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			exec := false
+			cmd := tokenCmdListTypes(&walletConfig{}, func(cmd *cobra.Command, config *walletConfig, kind twb.Kind) error {
+				require.Equal(t, tt.expectedKind, kind)
+				if len(tt.expectedPass) != 0 {
+					passwordFromArg, err := cmd.Flags().GetString(passwordArgCmdName)
+					require.NoError(t, err)
+					require.Equal(t, tt.expectedPass, passwordFromArg)
+				}
+				exec = true
+				return nil
+			})
+			cmd.SetArgs(tt.args)
+			err := cmd.Execute()
+			require.NoError(t, err)
+			require.True(t, exec)
+		})
+	}
 }
 
-func ensureUnit(t *testing.T, state tokens.TokenState, id *uint256.Int) *rma.Unit {
-	var unit *rma.Unit
-	require.Eventually(t, func() bool {
-		unit, err := state.GetUnit(id)
-		require.NoError(t, err)
-		require.NotNil(t, unit)
-		return true
-	}, test.WaitDuration, test.WaitTick)
+func TestWalletCreateFungibleTokenTypeCmd_SymbolFlag(t *testing.T) {
+	homedir := createNewTestWallet(t)
+	// missing symbol parameter
+	_, err := execCommand(homedir, "token new-type fungible --decimals 3")
+	require.ErrorContains(t, err, "required flag(s) \"symbol\" not set")
+	// symbol parameter not set
+	_, err = execCommand(homedir, "flag needs an argument: --symbol")
+	// there currently are no restrictions on symbol length on CLI side
+}
 
-	return unit
+func TestWalletCreateFungibleTokenTypeCmd_TypeIdlFlag(t *testing.T) {
+	homedir := createNewTestWallet(t)
+	// hidden parameter type (not a mandatory parameter)
+	_, err := execCommand(homedir, "token new-type fungible --symbol \"@1\" --type")
+	require.ErrorContains(t, err, "flag needs an argument: --type")
+	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --type 011")
+	require.ErrorContains(t, err, "invalid argument \"011\" for \"--type\" flag: encoding/hex: odd length hex string")
+	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --type foo")
+	require.ErrorContains(t, err, "invalid argument \"foo\" for \"--type\" flag")
+	// there currently are no restrictions on type length on CLI side
+}
+
+func TestWalletCreateFungibleTokenTypeCmd_DecimalsFlag(t *testing.T) {
+	homedir := createNewTestWallet(t)
+	// hidden parameter type (not a mandatory parameter)
+	_, err := execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals")
+	require.ErrorContains(t, err, "flag needs an argument: --decimals")
+	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals foo")
+	require.ErrorContains(t, err, "invalid argument \"foo\" for \"--decimals\" flag")
+	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals -1")
+	require.ErrorContains(t, err, "invalid argument \"-1\" for \"--decimals\"")
+	_, err = execCommand(homedir, "token new-type fungible --symbol \"@1\" --decimals 9")
+	require.ErrorContains(t, err, "argument \"9\" for \"--decimals\" flag is out of range, max value 8")
+}
+
+func TestWalletCreateFungibleTokenCmd_TypeFlag(t *testing.T) {
+	homedir := createNewTestWallet(t)
+	_, err := execCommand(homedir, "token new fungible --type A8B")
+	require.ErrorContains(t, err, "invalid argument \"A8B\" for \"--type\" flag: encoding/hex: odd length hex string")
+	_, err = execCommand(homedir, "token new fungible --type nothex")
+	require.ErrorContains(t, err, "invalid argument \"nothex\" for \"--type\" flag: encoding/hex: invalid byte")
+	_, err = execCommand(homedir, "token new fungible --amount 4")
+	require.ErrorContains(t, err, "required flag(s) \"type\" not set")
+}
+
+func TestWalletCreateFungibleTokenCmd_AmountFlag(t *testing.T) {
+	homedir := createNewTestWallet(t)
+	_, err := execCommand(homedir, "token new fungible --type A8BB")
+	require.ErrorContains(t, err, "required flag(s) \"amount\" not set")
+}
+
+func TestWalletCreateNonFungibleTokenCmd_TypeFlag(t *testing.T) {
+	type args struct {
+		cmdParams string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       []byte
+		wantErrStr string
+	}{
+		{
+			name:       "missing token type parameter",
+			args:       args{cmdParams: "token new non-fungible --data 12AB"},
+			wantErrStr: "required flag(s) \"type\" not set",
+		},
+		{
+			name:       "missing token type parameter has no value",
+			args:       args{cmdParams: "token new non-fungible --type"},
+			wantErrStr: "flag needs an argument: --type",
+		},
+		{
+			name:       "type parameter is not hex encoded",
+			args:       args{cmdParams: "token new non-fungible --type 11dummy"},
+			wantErrStr: "invalid argument \"11dummy\" for \"--type\" flag",
+		},
+		{
+			name:       "type parameter is odd length",
+			args:       args{cmdParams: "token new non-fungible --type A8B08"},
+			wantErrStr: "invalid argument \"A8B08\" for \"--type\" flag: encoding/hex: odd length hex string",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			homedir := createNewTestWallet(t)
+			_, err := execCommand(homedir, tt.args.cmdParams)
+			if len(tt.wantErrStr) != 0 {
+				require.ErrorContains(t, err, tt.wantErrStr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestWalletCreateNonFungibleTokenCmd_TokenIdFlag(t *testing.T) {
+	//token-identifier parameter is odd length
+	homedir := createNewTestWallet(t)
+	_, err := execCommand(homedir, "token new non-fungible --type A8B0 --token-identifier A8B09")
+	require.ErrorContains(t, err, "invalid argument \"A8B09\" for \"--token-identifier\" flag: encoding/hex: odd length hex string")
+	_, err = execCommand(homedir, "token new non-fungible --type A8B0 --token-identifier nothex")
+	require.ErrorContains(t, err, "invalid argument \"nothex\" for \"--token-identifier\" flag: encoding/hex: invalid byte")
+}
+
+func TestWalletCreateNonFungibleTokenCmd_DataFileFlag(t *testing.T) {
+	data := make([]byte, maxBinaryFile64Kb+1)
+	tmpfile, err := ioutil.TempFile(t.TempDir(), "test")
+	require.NoError(t, err)
+	_, err = tmpfile.Write(data)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		cmdParams  string
+		want       []byte
+		wantErrStr string
+	}{
+		{
+			name:       "both data and data-file specified",
+			cmdParams:  "token new non-fungible --type 12AB --data 1122aabb --data-file=/tmp/test/foo.bin",
+			wantErrStr: "if any flags in the group [data data-file] are set none of the others can be; [data data-file] were all set",
+		},
+		{
+			name:       "data-file not found",
+			cmdParams:  "token new non-fungible --type 12AB --data-file=/tmp/test/foo.bin",
+			wantErrStr: "data-file read error: stat /tmp/test/foo.bin: no such file or directory",
+		},
+		{
+			name:       "data-file too big",
+			cmdParams:  "token new non-fungible --type 12AB --data-file=" + tmpfile.Name(),
+			wantErrStr: "data-file read error: file size over 64Kb limit",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			homedir := createNewTestWallet(t)
+			_, err := execCommand(homedir, tt.cmdParams)
+			if len(tt.wantErrStr) != 0 {
+				require.ErrorContains(t, err, tt.wantErrStr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestWalletUpdateNonFungibleTokenDataCmd_Flags(t *testing.T) {
+	data := make([]byte, maxBinaryFile64Kb+1)
+	tmpfile, err := ioutil.TempFile(t.TempDir(), "test")
+	require.NoError(t, err)
+	_, err = tmpfile.Write(data)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		cmdParams  string
+		want       []byte
+		wantErrStr string
+	}{
+		{
+			name:       "both data and data-file specified",
+			cmdParams:  "token update --token-identifier 12AB --data 1122aabb --data-file=/tmp/test/foo.bin",
+			wantErrStr: "if any flags in the group [data data-file] are set none of the others can be; [data data-file] were all set",
+		},
+		{
+			name:       "data-file not found",
+			cmdParams:  "token update --token-identifier 12AB --data-file=/tmp/test/foo.bin",
+			wantErrStr: "data-file read error: stat /tmp/test/foo.bin: no such file or directory",
+		},
+		{
+			name:       "data-file too big",
+			cmdParams:  "token update --token-identifier 12AB --data-file=" + tmpfile.Name(),
+			wantErrStr: "data-file read error: file size over 64Kb limit",
+		},
+		{
+			name:       "update nft: both data flags missing",
+			cmdParams:  "token update --token-identifier 12AB",
+			wantErrStr: "either of ['--data', '--data-file'] flags must be specified",
+		},
+		{
+			name:       "update nft: token id missing",
+			cmdParams:  "token update",
+			wantErrStr: "required flag(s) \"token-identifier\" not set",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			homedir := createNewTestWallet(t)
+			_, err := execCommand(homedir, tt.cmdParams)
+			if len(tt.wantErrStr) != 0 {
+				require.ErrorContains(t, err, tt.wantErrStr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
 
 // tokenID == nil means first token will be considered as success
@@ -723,68 +918,6 @@ func randomID(t *testing.T) []byte {
 	return id
 }
 
-func TestListTokensTypesCommandInputs(t *testing.T) {
-	tests := []struct {
-		name         string
-		args         []string
-		expectedKind twb.Kind
-		expectedPass string
-	}{
-		{
-			name:         "list all tokens",
-			args:         []string{},
-			expectedKind: twb.Any,
-		},
-		{
-			name:         "list all tokens, encrypted wallet",
-			args:         []string{"--pn", "test pass phrase"},
-			expectedKind: twb.Any,
-			expectedPass: "test pass phrase",
-		},
-		{
-			name:         "list all fungible tokens",
-			args:         []string{"fungible"},
-			expectedKind: twb.Fungible,
-		},
-		{
-			name:         "list all fungible tokens, encrypted wallet",
-			args:         []string{"fungible", "--pn", "test pass phrase"},
-			expectedKind: twb.Fungible,
-			expectedPass: "test pass phrase",
-		},
-		{
-			name:         "list all non-fungible tokens",
-			args:         []string{"non-fungible"},
-			expectedKind: twb.NonFungible,
-		},
-		{
-			name:         "list all non-fungible tokens, encrypted wallet",
-			args:         []string{"non-fungible", "--pn", "test pass phrase"},
-			expectedKind: twb.NonFungible,
-			expectedPass: "test pass phrase",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			exec := false
-			cmd := tokenCmdListTypes(&walletConfig{}, func(cmd *cobra.Command, config *walletConfig, kind twb.Kind) error {
-				require.Equal(t, tt.expectedKind, kind)
-				if len(tt.expectedPass) != 0 {
-					passwordFromArg, err := cmd.Flags().GetString(passwordArgCmdName)
-					require.NoError(t, err)
-					require.Equal(t, tt.expectedPass, passwordFromArg)
-				}
-				exec = true
-				return nil
-			})
-			cmd.SetArgs(tt.args)
-			err := cmd.Execute()
-			require.NoError(t, err)
-			require.True(t, exec)
-		})
-	}
-}
-
 func verifyStdoutEventually(t *testing.T, exec func() *testConsoleWriter, expectedLines ...string) {
 	require.Eventually(t, func() bool {
 		joined := strings.Join(exec().lines, "\n")
@@ -794,4 +927,53 @@ func verifyStdoutEventually(t *testing.T, exec func() *testConsoleWriter, expect
 		}
 		return res
 	}, test.WaitDuration, test.WaitTick)
+}
+
+type accountManagerMock struct {
+	keyHash       []byte
+	recordedIndex uint64
+}
+
+func (a *accountManagerMock) GetAccountKey(accountIndex uint64) (*account.AccountKey, error) {
+	a.recordedIndex = accountIndex
+	return &account.AccountKey{PubKeyHash: &account.KeyHashes{Sha256: a.keyHash}}, nil
+}
+
+func (a *accountManagerMock) GetAll() []account.Account {
+	return nil
+}
+
+func (a *accountManagerMock) CreateKeys(mnemonic string) error {
+	return nil
+}
+
+func (a *accountManagerMock) AddAccount() (uint64, []byte, error) {
+	return 0, nil, nil
+}
+
+func (a *accountManagerMock) GetMnemonic() (string, error) {
+	return "", nil
+}
+
+func (a *accountManagerMock) GetAccountKeys() ([]*account.AccountKey, error) {
+	return nil, nil
+}
+
+func (a *accountManagerMock) GetMaxAccountIndex() (uint64, error) {
+	return 0, nil
+}
+
+func (a *accountManagerMock) GetPublicKey(accountIndex uint64) ([]byte, error) {
+	return nil, nil
+}
+
+func (a *accountManagerMock) GetPublicKeys() ([][]byte, error) {
+	return nil, nil
+}
+
+func (a *accountManagerMock) IsEncrypted() (bool, error) {
+	return false, nil
+}
+
+func (a *accountManagerMock) Close() {
 }

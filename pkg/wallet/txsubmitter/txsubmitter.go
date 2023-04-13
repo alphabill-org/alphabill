1430cefe3aa35f058ba9962492999c18aa3c897b
Pavel Grigorenko <pgrigorenko@gmail.com>
2023-04-13 15:43:31 +0200

    extracted tx submitter

---
+++ pkg/wallet/txsubmitter/txsubmitter.go
@@ -0,0 +1,117 @@
+package txsubmitter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/alphabill-org/alphabill/internal/txsystem"
+	"github.com/alphabill-org/alphabill/pkg/wallet"
+	"github.com/alphabill-org/alphabill/pkg/wallet/log"
+)
+
+type (
+	TxSubmission struct {
+		UnitID      wallet.UnitID
+		TxHash      wallet.TxHash
+		Transaction *txsystem.Transaction
+		Confirmed   bool
+	}
+
+	TxSubmissionBatch struct {
+		Sender      wallet.PubKey
+		submissions []*TxSubmission
+		maxTimeout  uint64
+		Backend     BackendAPI
+	}
+
+	BackendAPI interface {
+		GetRoundNumber(ctx context.Context) (uint64, error)
+		PostTransactions(ctx context.Context, pubKey wallet.PubKey, txs *txsystem.Transactions) error
+		GetTxProof(ctx context.Context, unitID wallet.UnitID, txHash wallet.TxHash) (*wallet.Proof, error)
+	}
+)
+
+func (s *TxSubmission) ToBatch(backend BackendAPI, sender wallet.PubKey) *TxSubmissionBatch {
+	return &TxSubmissionBatch{
+		Sender:      sender,
+		Backend:     backend,
+		submissions: []*TxSubmission{s},
+		maxTimeout:  s.Transaction.Timeout,
+	}
+}
+
+func (t *TxSubmissionBatch) Add(sub *TxSubmission) {
+	t.submissions = append(t.submissions, sub)
+	if sub.Transaction.Timeout > t.maxTimeout {
+		t.maxTimeout = sub.Transaction.Timeout
+	}
+}
+
+func (t *TxSubmissionBatch) transactions() []*txsystem.Transaction {
+	txs := make([]*txsystem.Transaction, 0, len(t.submissions))
+	for _, sub := range t.submissions {
+		txs = append(txs, sub.Transaction)
+	}
+	return txs
+}
+
+func (t *TxSubmissionBatch) SendTx(ctx context.Context, confirmTx bool) error {
+	if len(t.submissions) == 0 {
+		return errors.New("no transactions to send")
+	}
+	err := t.Backend.PostTransactions(ctx, t.Sender, &txsystem.Transactions{Transactions: t.transactions()})
+	if err != nil {
+		return err
+	}
+	if confirmTx {
+		return t.ConfirmUnitsTx(ctx)
+	}
+	return nil
+}
+
+func (t *TxSubmissionBatch) ConfirmUnitsTx(ctx context.Context) error {
+	log.Info("Confirming submitted transactions")
+
+	for {
+		if err := ctx.Err(); err != nil {
+			return fmt.Errorf("confirming transactions interrupted: %w", err)
+		}
+
+		roundNr, err := t.Backend.GetRoundNumber(ctx)
+		if err != nil {
+			return err
+		}
+		if roundNr >= t.maxTimeout {
+			log.Info(fmt.Sprintf("Tx confirmation timeout is reached, block (#%v)", roundNr))
+			for _, sub := range t.submissions {
+				if !sub.Confirmed {
+					log.Info(fmt.Sprintf("Tx not confirmed for UnitID=%X", sub.UnitID))
+				}
+			}
+			return errors.New("confirmation timeout")
+		}
+		unconfirmed := false
+		for _, sub := range t.submissions {
+			if sub.Confirmed || roundNr >= sub.Transaction.Timeout {
+				continue
+			}
+			proof, err := t.Backend.GetTxProof(ctx, sub.UnitID, sub.TxHash)
+			if err != nil {
+				return err
+			}
+			if proof != nil {
+				log.Debug(fmt.Sprintf("UnitID=%X is confirmed", sub.UnitID))
+				sub.Confirmed = true
+			}
+			unconfirmed = unconfirmed || !sub.Confirmed
+		}
+		if unconfirmed {
+			time.Sleep(500 * time.Millisecond)
+		} else {
+			log.Info("All transactions confirmed")
+			return nil
+		}
+	}
+}

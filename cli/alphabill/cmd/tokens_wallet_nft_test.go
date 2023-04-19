8a0d57e202c90a86e59ad96f156b5b028208803f
Pavel Grigorenko <pgrigorenko@gmail.com>
2023-04-19 19:21:41 +0200

    first, failing test

--- cli/alphabill/cmd/tokens_wallet_nft_test.go
+++ cli/alphabill/cmd/tokens_wallet_nft_test.go
@@ -25,7 +25,7 @@ func TestNFTs_Integration(t *testing.T) {
 	backendUrl, client, ctx := startTokensBackend(t, nodeDialUrl)
 
 	w1, homedirW1 := createNewTokenWallet(t, backendUrl)
-	w1key, err := w1.GetAccountManager().GetAccountKey(0)
+	_, w1key2, err := w1.GetAccountManager().AddAccount()
 	require.NoError(t, err)
 	w1.Shutdown()
 
@@ -40,35 +40,35 @@ func TestNFTs_Integration(t *testing.T) {
 	nftID := randomID(t)
 	symbol := "ABNFT"
 	execTokensCmdWithError(t, homedirW1, "new-type non-fungible", "required flag(s) \"symbol\" not set")
-	execTokensCmd(t, homedirW1, fmt.Sprintf("new-type non-fungible --symbol %s -r %s --type %X --subtype-clause ptpkh", symbol, backendUrl, typeID))
-	ensureTokenTypeIndexed(t, ctx, client, w1key.PubKey, typeID)
-	execTokensCmd(t, homedirW1, fmt.Sprintf("new-type non-fungible --symbol %s -r %s --type %X --parent-type %X --subtype-input ptpkh", symbol+"2", backendUrl, typeID2, typeID))
-	ensureTokenTypeIndexed(t, ctx, client, w1key.PubKey, typeID2)
+	execTokensCmd(t, homedirW1, fmt.Sprintf("new-type non-fungible -k 2 --symbol %s -r %s --type %X --subtype-clause ptpkh", symbol, backendUrl, typeID))
+	ensureTokenTypeIndexed(t, ctx, client, w1key2, typeID)
+	execTokensCmd(t, homedirW1, fmt.Sprintf("new-type non-fungible -k 2 --symbol %s -r %s --type %X --parent-type %X --subtype-input ptpkh", symbol+"2", backendUrl, typeID2, typeID))
+	ensureTokenTypeIndexed(t, ctx, client, w1key2, typeID2)
 	// mint NFT
-	execTokensCmd(t, homedirW1, fmt.Sprintf("new non-fungible -r %s --type %X --token-identifier %X", backendUrl, typeID, nftID))
+	execTokensCmd(t, homedirW1, fmt.Sprintf("new non-fungible -k 2 -r %s --type %X --token-identifier %X", backendUrl, typeID, nftID))
 	require.Eventually(t, testpartition.BlockchainContains(partition, func(tx *txsystem.Transaction) bool {
 		return tx.TransactionAttributes.GetTypeUrl() == "type.googleapis.com/alphabill.tokens.v1.MintNonFungibleTokenAttributes" && bytes.Equal(tx.UnitId, nftID)
 	}), test.WaitDuration, test.WaitTick)
-	ensureTokenIndexed(t, ctx, client, w1key.PubKey, nftID)
+	ensureTokenIndexed(t, ctx, client, w1key2, nftID)
 	// transfer NFT
-	execTokensCmd(t, homedirW1, fmt.Sprintf("send non-fungible -r %s --token-identifier %X --address 0x%X -k 1", backendUrl, nftID, w2key.PubKey))
+	execTokensCmd(t, homedirW1, fmt.Sprintf("send non-fungible -k 2 -r %s --token-identifier %X --address 0x%X", backendUrl, nftID, w2key.PubKey))
 	require.Eventually(t, testpartition.BlockchainContains(partition, func(tx *txsystem.Transaction) bool {
 		return tx.TransactionAttributes.GetTypeUrl() == "type.googleapis.com/alphabill.tokens.v1.TransferNonFungibleTokenAttributes" && bytes.Equal(tx.UnitId, nftID)
 	}), test.WaitDuration, test.WaitTick)
 	ensureTokenIndexed(t, ctx, client, w2key.PubKey, nftID)
 	verifyStdout(t, execTokensCmd(t, homedirW2, fmt.Sprintf("list non-fungible -r %s", backendUrl)), fmt.Sprintf("ID='%X'", nftID))
 	//check what is left in w1, nothing, that is
-	verifyStdout(t, execTokensCmd(t, homedirW1, fmt.Sprintf("list non-fungible -r %s", backendUrl)), "No tokens")
+	verifyStdout(t, execTokensCmd(t, homedirW1, fmt.Sprintf("list non-fungible -k 2 -r %s", backendUrl)), "No tokens")
 	// list token types
 	verifyStdout(t, execTokensCmd(t, homedirW1, fmt.Sprintf("list-types -r %s", backendUrl)), "symbol=ABNFT (nft)")
 	verifyStdout(t, execTokensCmd(t, homedirW1, fmt.Sprintf("list-types non-fungible -r %s", backendUrl)), "symbol=ABNFT (nft)")
 	// transfer back
-	execTokensCmd(t, homedirW2, fmt.Sprintf("send non-fungible -r %s --token-identifier %X --address 0x%X -k 1", backendUrl, nftID, w1key.PubKey))
-	ensureTokenIndexed(t, ctx, client, w1key.PubKey, nftID)
+	execTokensCmd(t, homedirW2, fmt.Sprintf("send non-fungible -r %s --token-identifier %X --address 0x%X -k 1", backendUrl, nftID, w1key2))
+	ensureTokenIndexed(t, ctx, client, w1key2, nftID)
 	// mint nft from w1 and set the owner to w2
 	nftID2 := randomID(t)
 	verifyStdout(t, execTokensCmd(t, homedirW2, fmt.Sprintf("list non-fungible -r %s", backendUrl)), "No tokens")
-	execTokensCmd(t, homedirW1, fmt.Sprintf("new non-fungible  -r %s --type %X --bearer-clause ptpkh:0x%X --token-identifier %X", backendUrl, typeID, w2key.PubKeyHash.Sha256, nftID2))
+	execTokensCmd(t, homedirW1, fmt.Sprintf("new non-fungible -k 2 -r %s --type %X --bearer-clause ptpkh:0x%X --token-identifier %X", backendUrl, typeID, w2key.PubKeyHash.Sha256, nftID2))
 	verifyStdout(t, execTokensCmd(t, homedirW2, fmt.Sprintf("list non-fungible -r %s", backendUrl)), fmt.Sprintf("ID='%X'", nftID2))
 }
 

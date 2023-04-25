198dfa40fb4f5acb14203e004fb645504782e57b
Ain <ain.valtin@guardtime.com>
2023-04-25 11:01:18 +0300

    fix bogus timeout; make ProtobufWriter.Write argument strongly typed

--- internal/network/send_protocol.go
+++ internal/network/send_protocol.go
@@ -28,18 +28,17 @@ func (p *SendProtocol) ID() string {
 
 func (p *SendProtocol) Send(m proto.Message, receiverID peer.ID) error {
 	ctx, cancel := context.WithTimeout(context.Background(), p.timeout)
+	defer cancel()
 	doneCh := make(chan error, 1)
+
 	go func() {
-		defer close(doneCh)
-		defer cancel()
 		s, err := p.self.CreateStream(ctx, receiverID, p.protocolID)
 		if err != nil {
 			doneCh <- fmt.Errorf("open stream error: %w", err)
 			return
 		}
 		defer func() {
-			err := s.Close()
-			if err != nil {
+			if err := s.Close(); err != nil {
 				logger.Warning("Failed to close libp2p stream, error: %v, "+
 					"protocol: %s, receiver peerID: %v, sender peerID: %v", err, p.protocolID, receiverID, p.self.ID())
 			}
@@ -47,14 +46,13 @@ func (p *SendProtocol) Send(m proto.Message, receiverID peer.ID) error {
 
 		w := NewProtoBufWriter(s)
 		defer func() {
-			err := w.Close()
-			if err != nil {
+			if err := w.Close(); err != nil {
 				logger.Warning("Failed to close protobuf writer, error: %v, "+
 					"protocol: %s, receiver peerID: %v, sender peerID: %v", err, p.protocolID, receiverID, p.self.ID())
 			}
 		}()
-		err = w.Write(m)
-		if err != nil {
+
+		if err := w.Write(m); err != nil {
 			doneCh <- fmt.Errorf("write error: %w", err)
 		}
 		doneCh <- nil

80d8410a1b7f218d61b59d7d2c54d2d3ba586969
Lennart Ploom <lennart.ploom@guardtime.com>
2023-05-19 18:50:48 +0300

    add fee txs to token tx converter

--- pkg/wallet/tokens/tx_converter.go
+++ pkg/wallet/tokens/tx_converter.go
@@ -2,6 +2,7 @@ package tokens
 
 import (
 	"github.com/alphabill-org/alphabill/internal/txsystem"
+	"github.com/alphabill-org/alphabill/internal/txsystem/fc/transactions"
 	"github.com/alphabill-org/alphabill/internal/txsystem/tokens"
 )
 
@@ -9,5 +10,12 @@ type TokenTxConverter struct {
 }
 
 func (*TokenTxConverter) ConvertTx(tx *txsystem.Transaction) (txsystem.GenericTransaction, error) {
+	feeTx, err := transactions.NewPartitionFeeCreditTx(tx)
+	if err != nil {
+		return nil, err
+	}
+	if feeTx != nil {
+		return feeTx, nil
+	}
 	return tokens.NewGenericTx(tx)
 }

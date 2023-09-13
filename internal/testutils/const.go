d7471225615e7ba1435765f55bd6634550217849
Mait Märdin <mait.mardin@guardtime.com>
2023-09-13 11:03:14 +0300

    AB-1160 Flaky test 1. Just one 100ms tick in 200ms timeout.

--- internal/testutils/const.go
+++ internal/testutils/const.go
@@ -3,6 +3,7 @@ package test
 import "time"
 
 const (
-	WaitDuration = 4 * time.Second
-	WaitTick     = 100 * time.Millisecond
+	WaitDuration  = 4 * time.Second
+	WaitShortTick = 50 * time.Millisecond
+	WaitTick      = 100 * time.Millisecond
 )

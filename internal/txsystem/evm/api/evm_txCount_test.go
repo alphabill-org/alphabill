2a1e9b87c290f8ad68726138489cb46651a8369a
Kristjan Tõiste <kristjan.toiste@guardtime.com>
2023-09-14 10:48:25 +0300

    Added /evm/transactionCount endpoint
    Changes:
    - added endpoint to get nonce from EVM account

---
+++ internal/txsystem/evm/api/evm_txCount_test.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"fmt"
+	"math/big"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/alphabill-org/alphabill/internal/rpc"
+	abstate "github.com/alphabill-org/alphabill/internal/state"
+	test "github.com/alphabill-org/alphabill/internal/testutils"
+	"github.com/alphabill-org/alphabill/internal/txsystem/evm/statedb"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/fxamacker/cbor/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAPI_TransactionCount_OK(t *testing.T) {
+	tree := abstate.NewEmptyState()
+	stateDB := statedb.NewStateDB(tree)
+	address := common.BytesToAddress(test.RandomBytes(20))
+
+	stateDB.CreateAccount(address)
+	stateDB.SetNonce(address, 333)
+	_, _, err := tree.CalculateRoot()
+	require.NoError(t, err)
+	require.NoError(t, tree.Commit())
+
+	a := &API{
+		state:            tree,
+		systemIdentifier: []byte{0, 0, 0, 1},
+		gasUnitPrice:     big.NewInt(10),
+		blockGasLimit:    10000,
+	}
+
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/evm/transactionCount/%X", address.Bytes()), nil)
+	recorder := httptest.NewRecorder()
+
+	rpc.NewRESTServer("", 2000, a).Handler.ServeHTTP(recorder, req)
+	require.Equal(t, http.StatusOK, recorder.Code)
+	resp := &struct {
+		_     struct{} `cbor:",toarray"`
+		Nonce uint64
+	}{}
+	require.NoError(t, cbor.NewDecoder(recorder.Body).Decode(resp))
+	require.EqualValues(t, 333, resp.Nonce)
+}

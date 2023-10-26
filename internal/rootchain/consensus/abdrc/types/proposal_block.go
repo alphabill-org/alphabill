5ab2626495f588fb3d878523d46b8669eb8233b2
Kristjan Tõiste <kristjan.toiste@guardtime.com>
2023-10-26 16:43:12 +0300

    Improved block data stringer output

--- internal/rootchain/consensus/abdrc/types/proposal_block.go
+++ internal/rootchain/consensus/abdrc/types/proposal_block.go
@@ -131,11 +131,11 @@ func (x *BlockData) GetParentRound() uint64 {
 // Summary - stringer returns a payload summary
 func (x *BlockData) String() string {
 	if x.Payload == nil || len(x.Payload.Requests) == 0 {
-		return fmt.Sprintf("round %v, time %v, payload(empty)", x.Round, x.Timestamp)
+		return fmt.Sprintf("round: %v, time: %v, payload: empty", x.Round, x.Timestamp)
 	}
 	var changed []string
 	for _, req := range x.Payload.Requests {
 		changed = append(changed, fmt.Sprintf("%s", req))
 	}
-	return fmt.Sprintf("round %v, time %v, payload(%s)", x.Round, x.Timestamp, strings.Join(changed, ", "))
+	return fmt.Sprintf("round: %v, time: %v, payload: %s", x.Round, x.Timestamp, strings.Join(changed, ", "))
 }

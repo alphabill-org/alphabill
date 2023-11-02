5c131d68bb36e8c6281aa3186042bc8224fb2d42
Kristjan Tõiste <kristjan.toiste@guardtime.com>
2023-11-02 10:16:15 +0200

    AB-1290
    Changes:
    - Run root node with one host and have the parameters similar to
      partition node.

--- cli/alphabill/cmd/rootchain.go
+++ cli/alphabill/cmd/rootchain.go
@@ -6,11 +6,9 @@ import (
 	"fmt"
 	"log/slog"
 	"path/filepath"
-	"sort"
 	"strings"
 	"time"
 
-	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 	"github.com/spf13/cobra"
@@ -40,8 +38,7 @@ type rootNodeConfig struct {
 	Base               *baseConfiguration
 	KeyFile            string // path to rootchain chain key file
 	GenesisFile        string // path to rootchain-genesis.json file
-	PartitionListener  string // partition validator node address (libp2p multiaddress format)
-	RootListener       string // Root validator node address (libp2p multiaddress format)
+	Address            string // node address (libp2p multiaddress format)
 	StoragePath        string // path to Bolt storage file
 	MaxRequests        uint   // validator partition certification request channel capacity
 	BootStrapAddresses string // boot strap addresses (libp2p multiaddress format)
@@ -63,8 +60,7 @@ func newRootNodeCmd(baseConfig *baseConfiguration) *cobra.Command {
 	cmd.Flags().StringVarP(&config.KeyFile, keyFileCmdFlag, "k", "", "path to node validator key file  (default $AB_HOME/rootchain/"+defaultKeysFileName+")")
 	cmd.Flags().StringVar(&config.GenesisFile, "genesis-file", "", "path to root-genesis.json file (default $AB_HOME/rootchain/"+rootGenesisFileName+")")
 	cmd.Flags().StringVar(&config.StoragePath, "db", "", "persistent store path (default: $AB_HOME/rootchain/)")
-	cmd.Flags().StringVar(&config.PartitionListener, "partition-listener", "/ip4/127.0.0.1/tcp/26662", "validator address in libp2p multiaddress-format")
-	cmd.Flags().StringVar(&config.RootListener, rootPortCmdFlag, "/ip4/127.0.0.1/tcp/29666", "validator address in libp2p multiaddress-format")
+	cmd.Flags().StringVar(&config.Address, "address", "/ip4/127.0.0.1/tcp/26662", "validator address in libp2p multiaddress-format")
 	cmd.Flags().UintVar(&config.MaxRequests, "max-requests", 1000, "request buffer capacity")
 	cmd.Flags().StringVar(&config.BootStrapAddresses, rootBootStrapNodesCmdFlag, "", "comma separated list of bootstrap root node addresses id@libp2p-multiaddress-format")
 	return cmd
@@ -150,12 +146,12 @@ func defaultRootNodeRunFunc(ctx context.Context, config *rootNodeConfig) error {
 		return fmt.Errorf("root genesis verification failed: %w", err)
 	}
 	// Process partition node network
-	prtHost, err := createHost(ctx, config.PartitionListener, keys.EncryptionPrivateKey, config.Base.Logger)
+	host, err := createHost(ctx, keys, config, config.Base.Logger)
 	if err != nil {
 		return fmt.Errorf("creating partition host: %w", err)
 	}
-	log := config.Base.Logger.With(logger.NodeID(prtHost.ID()))
-	partitionNet, err := network.NewLibP2PRootChainNetwork(prtHost, config.MaxRequests, defaultNetworkTimeout, log)
+	log := config.Base.Logger.With(logger.NodeID(host.ID()))
+	partitionNet, err := network.NewLibP2PRootChainNetwork(host, config.MaxRequests, defaultNetworkTimeout, log)
 	if err != nil {
 		return fmt.Errorf("partition network initialization failed: %w", err)
 	}
@@ -163,7 +159,7 @@ func defaultRootNodeRunFunc(ctx context.Context, config *rootNodeConfig) error {
 	if err != nil {
 		return fmt.Errorf("invalid root node sign key: %w", err)
 	}
-	if err := verifyKeyPresentInGenesis(prtHost, rootGenesis.Root, ver); err != nil {
+	if err = verifyKeyPresentInGenesis(host, rootGenesis.Root, ver); err != nil {
 		return fmt.Errorf("root node key not found in genesis: %w", err)
 	}
 	// initiate partition store
@@ -180,26 +176,20 @@ func defaultRootNodeRunFunc(ctx context.Context, config *rootNodeConfig) error {
 	if len(rootGenesis.Root.RootValidators) == 1 {
 		// use monolithic consensus algorithm
 		cm, err = monolithic.NewMonolithicConsensusManager(
-			prtHost.ID().String(),
+			host.ID().String(),
 			rootGenesis,
 			partitionCfg,
 			keys.SigningPrivateKey,
 			log,
 			consensus.WithStorage(store))
 	} else {
-		// Initiate Root validator network
-		var rootHost *network.Peer
-		rootHost, err = createConsensusHost(ctx, keys, rootGenesis.Root.RootValidators, config, log)
-		if err != nil {
-			return fmt.Errorf("failed to create root node host, %w", err)
-		}
 		var rootNet *network.LibP2PNetwork
-		rootNet, err = network.NewLibP2RootConsensusNetwork(rootHost, config.MaxRequests, defaultNetworkTimeout, log)
+		rootNet, err = network.NewLibP2RootConsensusNetwork(host, config.MaxRequests, defaultNetworkTimeout, log)
 		if err != nil {
 			return fmt.Errorf("failed initiate root network, %w", err)
 		}
 		// Create distributed consensus manager function
-		cm, err = abdrc.NewDistributedAbConsensusManager(rootHost.ID(),
+		cm, err = abdrc.NewDistributedAbConsensusManager(host.ID(),
 			rootGenesis,
 			partitionCfg,
 			rootNet,
@@ -211,7 +201,7 @@ func defaultRootNodeRunFunc(ctx context.Context, config *rootNodeConfig) error {
 		return fmt.Errorf("failed initiate consensus manager: %w", err)
 	}
 	node, err := rootchain.New(
-		prtHost,
+		host,
 		partitionNet,
 		partitionCfg,
 		cm,
@@ -222,63 +212,16 @@ func defaultRootNodeRunFunc(ctx context.Context, config *rootNodeConfig) error {
 	return node.Run(ctx)
 }
 
-func createConsensusHost(ctx context.Context, keys *Keys, rootValidators []*genesis.PublicKeyInfo, cfg *rootNodeConfig, log *slog.Logger) (*network.Peer, error) {
-	pair, err := keys.getEncryptionKeyPair()
-	if err != nil {
-		return nil, err
-	}
-	// read bootstrap parameter
+func createHost(ctx context.Context, keys *Keys, cfg *rootNodeConfig, log *slog.Logger) (*network.Peer, error) {
 	bootNodes, err := cfg.getBootStrapNodes()
 	if err != nil {
 		return nil, fmt.Errorf("boot nodes parameter error: %w", err)
 	}
-	selfId, err := peer.IDFromPublicKey(keys.EncryptionPrivateKey.GetPublic())
+	keyPair, err := keys.getEncryptionKeyPair()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("get key pair failed: %w", err)
 	}
-	var persistentPeerIDs = make(peer.IDSlice, len(rootValidators))
-	for i, validator := range rootValidators {
-		if selfId.String() == validator.NodeIdentifier {
-			if !bytes.Equal(pair.PublicKey, validator.EncryptionPublicKey) {
-				return nil, fmt.Errorf("invalid encryption key")
-			}
-			persistentPeerIDs[i] = selfId
-			continue
-		}
-		nodeID, err := network.NodeIDFromPublicKeyBytes(validator.EncryptionPublicKey)
-		if err != nil {
-			return nil, fmt.Errorf("invalid encryption key: %X", validator.EncryptionPublicKey)
-		}
-		if nodeID.String() != validator.NodeIdentifier {
-			return nil, fmt.Errorf("invalid nodeID/encryption key combination: %s", nodeID)
-		}
-		persistentPeerIDs[i] = nodeID
-	}
-	// Sort validators by public encryption key
-	sort.Sort(persistentPeerIDs)
-	conf := &network.PeerConfiguration{
-		Address:        cfg.RootListener,
-		KeyPair:        pair,
-		BootstrapPeers: bootNodes,
-		Validators:     persistentPeerIDs,
-	}
-	return network.NewPeer(ctx, conf, log)
-}
-
-func createHost(ctx context.Context, address string, encPrivate crypto.PrivKey, log *slog.Logger) (*network.Peer, error) {
-	privateKeyBytes, err := encPrivate.Raw()
-	if err != nil {
-		return nil, err
-	}
-	publicKeyBytes, err := encPrivate.GetPublic().Raw()
-	if err != nil {
-		return nil, err
-	}
-	keyPair := &network.PeerKeyPair{
-		PublicKey:  publicKeyBytes,
-		PrivateKey: privateKeyBytes,
-	}
-	peerConf, err := network.NewPeerConfiguration(address, keyPair, nil, nil)
+	peerConf, err := network.NewPeerConfiguration(cfg.Address, keyPair, bootNodes, nil)
 	if err != nil {
 		return nil, err
 	}

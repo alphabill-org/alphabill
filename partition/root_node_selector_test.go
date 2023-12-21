d3154b42ebc8d0262867ebde673f4933128a4455
Kristjan Tõiste <kristjan.toiste@guardtime.com>
2023-12-21 16:04:22 +0200

    Unit test corrections

--- partition/root_node_selector_test.go
+++ partition/root_node_selector_test.go
@@ -56,29 +56,35 @@ func Test_rootNodesSelector(t *testing.T) {
 	})
 }
 
-func Test_randomIndex(t *testing.T) {
+func Test_randomNodeSelector(t *testing.T) {
 	t.Run("root node list is nil", func(t *testing.T) {
 		var nodes peer.IDSlice = nil
-		rootNodes, err := randomNodeSelector(nodes, defaultHandshakeNodes)
+		rootNodes, err := randomNodeSelector(nodes, 3)
+		require.ErrorContains(t, err, "root node list is empty")
+		require.Nil(t, rootNodes)
+	})
+	t.Run("root node list is empty", func(t *testing.T) {
+		var nodes peer.IDSlice
+		rootNodes, err := randomNodeSelector(nodes, 3)
 		require.ErrorContains(t, err, "root node list is empty")
 		require.Nil(t, rootNodes)
 	})
 	t.Run("1 root node", func(t *testing.T) {
 		nodes := test.GeneratePeerIDs(t, 1)
-		rootNodes, err := randomNodeSelector(nodes, defaultHandshakeNodes)
+		rootNodes, err := randomNodeSelector(nodes, 2)
 		require.NoError(t, err)
-		require.NotNil(t, rootNodes)
+		require.Len(t, rootNodes, 1)
 	})
 	t.Run("choose 3 from 3 root nodes", func(t *testing.T) {
 		nodes := test.GeneratePeerIDs(t, 3)
-		rootNodes, err := randomNodeSelector(nodes, defaultHandshakeNodes)
+		rootNodes, err := randomNodeSelector(nodes, 3)
 		require.NoError(t, err)
 		require.Equal(t, nodes, rootNodes)
 		require.Len(t, rootNodes, 3)
 	})
 	t.Run("choose 3 from 10 root nodes", func(t *testing.T) {
 		nodes := test.GeneratePeerIDs(t, 10)
-		rootNodes, err := randomNodeSelector(nodes, defaultHandshakeNodes)
+		rootNodes, err := randomNodeSelector(nodes, 3)
 		require.NoError(t, err)
 		require.NotNil(t, rootNodes)
 		require.Len(t, rootNodes, 3)

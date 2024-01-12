21f505b588286aed07652d3b8d0d08c3c9d94b15
Kristjan Tõiste <kristjan.toiste@guardtime.com>
2024-01-12 14:30:55 +0200

    Indexed merkle tree clean-up

--- tree/imt/indexed_merkle_tree.go
+++ tree/imt/indexed_merkle_tree.go
@@ -9,15 +9,13 @@ import (
 
 var ErrIndexOutOfBounds = errors.New("index out of bounds")
 
-type NodeType byte
-
 const (
-	Leaf NodeType = 0
-	Node NodeType = 1
+	Leaf byte = 0
+	Node byte = 1
 )
 
 type (
-	MerkleTree struct {
+	IMT struct {
 		root       *node
 		dataLength int // number of leaves
 	}
@@ -42,44 +40,52 @@ type (
 		right *node
 		hash  []byte
 		index []byte
-		nType NodeType
 	}
 )
 
-func (p *Pair) Hash(hashAlgorithm crypto.Hash, t NodeType) []byte {
+func (n *node) isLeaf() bool {
+	if n.left == nil && n.right == nil {
+		return true
+	}
+	return false
+}
+
+func (p *Pair) Hash(hashAlgorithm crypto.Hash) []byte {
 	hasher := hashAlgorithm.New()
-	hasher.Write([]byte{byte(t)})
+	hasher.Write([]byte{Leaf})
 	hasher.Write(p.Index)
 	hasher.Write(p.Data.Hash(hashAlgorithm))
 	return hasher.Sum(nil)
 }
 
-// New creates a new canonical Merkle Tree.
-func New(hashAlgorithm crypto.Hash, data []Pair) *MerkleTree {
+// New creates a new indexed Merkle Tree.
+func New(hashAlgorithm crypto.Hash, data []Pair) (*IMT, error) {
 	if len(data) == 0 {
-		return &MerkleTree{root: nil, dataLength: 0}
+		return &IMT{root: nil, dataLength: 0}, nil
 	}
-	return &MerkleTree{root: createMerkleTree(data, hashAlgorithm), dataLength: len(data)}
+	// validate order - perhaps could just sort here instead?
+	for i := len(data) - 1; i > 0; i-- {
+		if bytes.Compare(data[i].Index, data[i-1].Index) != 1 {
+			return nil, fmt.Errorf("data not sorted by index in not strictly ascending order")
+		}
+	}
+	return &IMT{root: createMerkleTree(data, hashAlgorithm), dataLength: len(data)}, nil
 }
 
 // EvalMerklePath returns root hash calculated from the given leaf and path items
 func EvalMerklePath(merklePath []*PathItem, leaf Pair, hashAlgorithm crypto.Hash) []byte {
 	hasher := hashAlgorithm.New()
-	h := leaf.Hash(hashAlgorithm, Leaf)
-	if len(merklePath) == 0 {
-		return nil
-	}
-	merklePath = merklePath[1:]
+	h := leaf.Hash(hashAlgorithm)
 	for _, item := range merklePath {
 		if bytes.Compare(leaf.Index, item.Index) == 1 {
 			// index > item.Index is bigger - left link
-			hasher.Write([]byte{byte(Node)})
+			hasher.Write([]byte{Node})
 			hasher.Write(item.Index)
 			hasher.Write(item.Hash)
 			hasher.Write(h)
 		} else {
 			// index <= item.Index is smaller or equal right link
-			hasher.Write([]byte{byte(Node)})
+			hasher.Write([]byte{Node})
 			hasher.Write(item.Index)
 			hasher.Write(h)
 			hasher.Write(item.Hash)
@@ -91,28 +97,23 @@ func EvalMerklePath(merklePath []*PathItem, leaf Pair, hashAlgorithm crypto.Hash
 }
 
 // IndexTreeOutput calculates the output hash of the chain.
-func IndexTreeOutput(merklePath []*PathItem, index []byte, hashAlgorithm crypto.Hash) []byte {
-	if len(merklePath) == 0 {
-		return nil
-	}
-	h := merklePath[0].Hash
-
-	if len(merklePath) == 1 {
-		return h
+func IndexTreeOutput(merklePath []*PathItem, index []byte, data Data, hashAlgorithm crypto.Hash) []byte {
+	leaf := Pair{
+		Index: index,
+		Data:  data,
 	}
 	hasher := hashAlgorithm.New()
-
-	merklePath = merklePath[1:]
+	h := leaf.Hash(hashAlgorithm)
 	for _, item := range merklePath {
 		if bytes.Compare(index, item.Index) == 1 {
 			// index > item.Index is bigger - left link
-			hasher.Write([]byte{byte(Node)})
+			hasher.Write([]byte{Node})
 			hasher.Write(item.Index)
 			hasher.Write(item.Hash)
 			hasher.Write(h)
 		} else {
 			// index <= item.Index is smaller or equal right link
-			hasher.Write([]byte{byte(Node)})
+			hasher.Write([]byte{Node})
 			hasher.Write(item.Index)
 			hasher.Write(h)
 			hasher.Write(item.Hash)
@@ -124,7 +125,7 @@ func IndexTreeOutput(merklePath []*PathItem, index []byte, hashAlgorithm crypto.
 }
 
 // GetRootHash returns the root Hash of the Merkle Tree.
-func (s *MerkleTree) GetRootHash() []byte {
+func (s *IMT) GetRootHash() []byte {
 	if s.root == nil {
 		return nil
 	}
@@ -132,16 +133,13 @@ func (s *MerkleTree) GetRootHash() []byte {
 }
 
 // GetMerklePath extracts the merkle path from the given leaf to root.
-func (s *MerkleTree) GetMerklePath(leafIdx []byte) ([]*PathItem, error) {
+func (s *IMT) GetMerklePath(leafIdx []byte) ([]*PathItem, error) {
 	if s.root == nil {
 		return nil, fmt.Errorf("tree empty")
 	}
-	if s.root.nType == Leaf {
-		return []*PathItem{{Index: s.root.index, Hash: s.root.hash}}, nil
-	}
 	var z []*PathItem
 	curr := s.root
-	for {
+	for !curr.isLeaf() {
 		if bytes.Compare(leafIdx, curr.index) == 1 {
 			z = append([]*PathItem{{Index: curr.index, Hash: curr.left.hash}}, z...)
 			curr = curr.right
@@ -149,17 +147,12 @@ func (s *MerkleTree) GetMerklePath(leafIdx []byte) ([]*PathItem, error) {
 			z = append([]*PathItem{{Index: curr.index, Hash: curr.right.hash}}, z...)
 			curr = curr.left
 		}
-		// leaf is the last node
-		if curr.nType == Leaf {
-			z = append([]*PathItem{{Index: curr.index, Hash: curr.hash}}, z...)
-			break
-		}
 	}
 	return z, nil
 }
 
 // PrettyPrint returns human readable string representation of the Merkle Tree.
-func (s *MerkleTree) PrettyPrint() string {
+func (s *IMT) PrettyPrint() string {
 	if s.root == nil {
 		return "tree is empty"
 	}
@@ -173,7 +166,7 @@ func createMerkleTree(data []Pair, hashAlgorithm crypto.Hash) *node {
 		return &node{hash: make([]byte, hashAlgorithm.Size())}
 	}
 	if len(data) == 1 {
-		return &node{index: data[0].Index, nType: Leaf, hash: data[0].Hash(hashAlgorithm, Leaf)}
+		return &node{index: data[0].Index, hash: data[0].Hash(hashAlgorithm)}
 	}
 	m := (len(data) + 1) / 2
 	leftSub := data[:m]
@@ -181,14 +174,14 @@ func createMerkleTree(data []Pair, hashAlgorithm crypto.Hash) *node {
 	hasher := hashAlgorithm.New()
 	left := createMerkleTree(leftSub, hashAlgorithm)
 	right := createMerkleTree(rightSub, hashAlgorithm)
-	hasher.Write([]byte{byte(Node)})
+	hasher.Write([]byte{Node})
 	hasher.Write(leftSub[len(leftSub)-1].Index)
 	hasher.Write(left.hash)
 	hasher.Write(right.hash)
-	return &node{index: leftSub[len(leftSub)-1].Index, nType: Node, left: left, right: right, hash: hasher.Sum(nil)}
+	return &node{index: leftSub[len(leftSub)-1].Index, left: left, right: right, hash: hasher.Sum(nil)}
 }
 
-func (s *MerkleTree) output(node *node, prefix string, isTail bool, str *string) {
+func (s *IMT) output(node *node, prefix string, isTail bool, str *string) {
 	if node.right != nil {
 		newPrefix := prefix
 		if isTail {
--- tree/imt/indexed_merkle_tree_test.go
+++ tree/imt/indexed_merkle_tree_test.go
@@ -17,16 +17,31 @@ func (t TestData) Hash(hash crypto.Hash) []byte {
 	return t.hash
 }
 
+func TestIMTNilCases(t *testing.T) {
+	imt := &IMT{}
+	require.Nil(t, imt.GetRootHash())
+	require.EqualValues(t, "tree is empty", imt.PrettyPrint())
+	index := []byte{0}
+	path, err := imt.GetMerklePath(index)
+	require.EqualError(t, err, "tree empty")
+	require.Nil(t, path)
+	var data []Pair
+	hashAlgo := crypto.SHA256
+	require.EqualValues(t, &node{hash: make([]byte, hashAlgo.Size())}, createMerkleTree(data, hashAlgo))
+}
+
 func TestNewIMTWithNilData(t *testing.T) {
 	var data []Pair = nil
-	imt := New(crypto.SHA256, data)
+	imt, err := New(crypto.SHA256, data)
+	require.NoError(t, err)
 	require.NotNil(t, imt)
 	require.Nil(t, imt.GetRootHash())
 	require.Equal(t, 0, imt.dataLength)
 }
 
 func TestNewIMTWithEmptyData(t *testing.T) {
-	imt := New(crypto.SHA256, []Pair{})
+	imt, err := New(crypto.SHA256, []Pair{})
+	require.NoError(t, err)
 	require.NotNil(t, imt)
 	require.Nil(t, imt.GetRootHash())
 	require.Equal(t, 0, imt.dataLength)
@@ -39,10 +54,50 @@ func TestNewIMTWithSingleNode(t *testing.T) {
 			Data:  &TestData{hash: make([]byte, 32)},
 		},
 	}
-	mt := New(crypto.SHA256, data)
-	require.NotNil(t, mt)
-	require.NotNil(t, mt.GetRootHash())
-	require.Equal(t, data[0].Hash(crypto.SHA256, Leaf), mt.GetRootHash())
+	imt, err := New(crypto.SHA256, data)
+	require.NoError(t, err)
+	require.NotNil(t, imt)
+	require.NotNil(t, imt.GetRootHash())
+	require.Equal(t, data[0].Hash(crypto.SHA256), imt.GetRootHash())
+	path, err := imt.GetMerklePath(data[0].Index)
+	hash := IndexTreeOutput(path, data[0].Index, data[0].Data, crypto.SHA256)
+	require.Equal(t, hash, imt.GetRootHash())
+}
+
+func TestNewIMTUnsortedInput(t *testing.T) {
+	var data = []Pair{
+		{
+			Index: util.Uint32ToBytes(uint32(3)),
+			Data:  &TestData{hash: makeData(byte(1))},
+		},
+		{
+			Index: util.Uint32ToBytes(uint32(1)),
+			Data:  &TestData{hash: makeData(byte(3))},
+		},
+	}
+	imt, err := New(crypto.SHA256, data)
+	require.EqualError(t, err, "data not sorted by index in not strictly ascending order")
+	require.Nil(t, imt)
+}
+
+func TestNewIMTEqualIndexValues(t *testing.T) {
+	var data = []Pair{
+		{
+			Index: util.Uint32ToBytes(uint32(1)),
+			Data:  &TestData{hash: makeData(byte(1))},
+		},
+		{
+			Index: util.Uint32ToBytes(uint32(3)),
+			Data:  &TestData{hash: makeData(byte(3))},
+		},
+		{
+			Index: util.Uint32ToBytes(uint32(3)),
+			Data:  &TestData{hash: makeData(byte(4))},
+		},
+	}
+	imt, err := New(crypto.SHA256, data)
+	require.EqualError(t, err, "data not sorted by index in not strictly ascending order")
+	require.Nil(t, imt)
 }
 
 func TestNewIMTYellowpaperExample(t *testing.T) {
@@ -68,31 +123,32 @@ func TestNewIMTYellowpaperExample(t *testing.T) {
 			Data:  &TestData{hash: makeData(byte(10))},
 		},
 	}
-	imt := New(crypto.SHA256, data)
+	imt, err := New(crypto.SHA256, data)
+	require.NoError(t, err)
 	require.NotNil(t, imt)
 	require.EqualValues(t, "80F2E3A2A379B86535F3D876CA70D648A256F5D67EADFFC76D0AB6F099AF7664", fmt.Sprintf("%X", imt.GetRootHash()))
 	require.NotEmpty(t, imt.PrettyPrint())
-	index := util.Uint32ToBytes(uint32(10))
-	path, err := imt.GetMerklePath(index)
-	require.NoError(t, err)
-	require.Len(t, path, 3)
-	hash := IndexTreeOutput(path, index, crypto.SHA256)
-	require.EqualValues(t, hash, imt.GetRootHash())
 	for _, d := range data {
-		path, err = imt.GetMerklePath(d.Index)
+		path, err := imt.GetMerklePath(d.Index)
 		require.NoError(t, err)
-		hash = EvalMerklePath(path, d, crypto.SHA256)
+		hash := IndexTreeOutput(path, d.Index, d.Data, crypto.SHA256)
 		require.EqualValues(t, hash, imt.GetRootHash())
 	}
-	// non-inclusion
+	for _, d := range data {
+		path, err := imt.GetMerklePath(d.Index)
+		require.NoError(t, err)
+		hash := EvalMerklePath(path, d, crypto.SHA256)
+		require.EqualValues(t, hash, imt.GetRootHash())
+	}
+	// test non-inclusion
 	item := Pair{
 		Index: util.Uint32ToBytes(uint32(5)),
 		Data:  &TestData{hash: makeData(byte(5))},
 	}
-	path, err = imt.GetMerklePath(item.Index)
+	path, err := imt.GetMerklePath(item.Index)
 	require.NoError(t, err)
 
-	hash = EvalMerklePath(path, item, crypto.SHA256)
+	hash := EvalMerklePath(path, item, crypto.SHA256)
 	require.NotEqualValues(t, hash, imt.GetRootHash())
 }
 
@@ -104,7 +160,8 @@ func TestNewIMTWithOddNumberOfLeaves(t *testing.T) {
 			Data:  &TestData{hash: makeData(byte(i))},
 		}
 	}
-	imt := New(crypto.SHA256, data)
+	imt, err := New(crypto.SHA256, data)
+	require.NoError(t, err)
 	require.NotNil(t, imt)
 	require.EqualValues(t, "2C811B7D3DD4E7BBD0A9E0C98FFC08495D5C871A6B3B6AD331543889206E1771", fmt.Sprintf("%X", imt.GetRootHash()))
 	require.NotEmpty(t, imt.PrettyPrint())
@@ -118,7 +175,8 @@ func TestNewIMTWithEvenNumberOfLeaves(t *testing.T) {
 			Data:  &TestData{hash: makeData(byte(i))},
 		}
 	}
-	imt := New(crypto.SHA256, data)
+	imt, err := New(crypto.SHA256, data)
+	require.NoError(t, err)
 	require.NotNil(t, imt)
 	require.EqualValues(t, "E7A8501605679E35DBF5642EB8372623CD8258FB541680891A77DAF82236B4CE", fmt.Sprintf("%X", imt.GetRootHash()))
 	require.NotEmpty(t, imt.PrettyPrint())
@@ -138,19 +196,6 @@ func TestNewIMTWithEvenNumberOfLeaves(t *testing.T) {
 
 	hash := EvalMerklePath(path, item, crypto.SHA256)
 	require.NotEqualValues(t, hash, imt.GetRootHash())
-}
-
-func TestSingleNodeTreeMerklePath(t *testing.T) {
-	var data = []Pair{
-		{
-			Index: util.Uint32ToBytes(uint32(0)),
-			Data:  &TestData{hash: make([]byte, 32)},
-		},
-	}
-	imt := New(crypto.SHA256, data)
-	path, err := imt.GetMerklePath(data[0].Index)
-	require.NoError(t, err)
-	require.NotEmpty(t, path)
 }
 
 func makeData(firstByte byte) []byte {

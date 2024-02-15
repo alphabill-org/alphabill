6f57eb1c5b9a5216c71c5b52225c382a2a571451
Jonathan Hepp <jonathan.hepp@guardtime.com>
2024-02-15 09:36:45 +0200

    AB-1350: additional tx proof tests

--- types/tx_proof_test.go
+++ types/tx_proof_test.go
@@ -39,10 +39,27 @@ func TestTxProofFunctions(t *testing.T) {
 		UnicityCertificate: uc,
 	}
 
-	t.Run("Test NewTxProof", func(t *testing.T) {
-		proof, _, err := NewTxProof(block, 0, crypto.SHA256)
+	t.Run("Test NewTxProof OK", func(t *testing.T) {
+		proof, record, err := NewTxProof(block, 0, crypto.SHA256)
 		require.NoError(t, err)
-		require.NotNil(t, proof)
+		require.Equal(t, block.HeaderHash(crypto.SHA256), proof.BlockHeaderHash)
+		require.Empty(t, proof.Chain)
+		require.Equal(t, uc, proof.UnicityCertificate)
+		require.Equal(t, block.Transactions[0], record)
+	})
+
+	t.Run("Test NewTxProof nil block", func(t *testing.T) {
+		proof, record, err := NewTxProof(nil, 0, crypto.SHA256)
+		require.Nil(t, proof)
+		require.Nil(t, record)
+		require.ErrorContains(t, err, "block is nil")
+	})
+
+	t.Run("Test NewTxProof empty block", func(t *testing.T) {
+		proof, record, err := NewTxProof(&Block{}, 0, crypto.SHA256)
+		require.Nil(t, proof)
+		require.Nil(t, record)
+		require.ErrorContains(t, err, "invalid tx index")
 	})
 
 	t.Run("Test VerifyTxProof ", func(t *testing.T) {

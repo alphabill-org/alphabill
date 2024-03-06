990e31570b5fe2a4ef14e6767f978dd9f4b2b807
Jonathan Hepp <jonathan.hepp@guardtime.com>
2024-03-06 16:08:25 +0200

    AB-1350: coverage update

--- network/validator_network_test.go
+++ network/validator_network_test.go
@@ -1,16 +1,9 @@
 package network
 
 import (
-	"bytes"
-	"context"
 	"crypto"
-	"io"
-	"log/slog"
-	"os"
 	"testing"
 	"time"
-
-	"github.com/alphabill-org/alphabill/logger"
 
 	"github.com/alphabill-org/alphabill/internal/testutils/observability"
 	"github.com/libp2p/go-libp2p"
@@ -44,45 +37,4 @@ func TestNewLibP2PValidatorNetwork(t *testing.T) {
 	network, err := NewLibP2PValidatorNetwork(&Peer{host: h}, opts, observability.Default(t))
 	require.NoError(t, err)
 	require.NotNil(t, network)
-}
-
-func TestValidatorNetwork_ForwardTransactionEmpty(t *testing.T) {
-	opts := ValidatorNetworkOptions{
-		ReceivedChannelCapacity:          1000,
-		TxBufferSize:                     1000,
-		TxBufferHashAlgorithm:            crypto.SHA256,
-		BlockCertificationTimeout:        300 * time.Millisecond,
-		BlockProposalTimeout:             300 * time.Millisecond,
-		LedgerReplicationRequestTimeout:  300 * time.Millisecond,
-		LedgerReplicationResponseTimeout: 300 * time.Millisecond,
-		HandshakeTimeout:                 300 * time.Millisecond,
-	}
-
-	var buf bytes.Buffer
-	obs := observability.New(t, "", "", func(configuration *logger.LogConfiguration) (*slog.Logger, error) {
-		return slog.New(slog.NewTextHandler(io.MultiWriter(&buf, os.Stdout), nil)), nil
-	})
-
-	h, err := libp2p.New([]config.Option{
-		libp2p.ListenAddrStrings(defaultAddress),
-	}...)
-	require.NoError(t, err)
-	defer func() {
-		err := h.Close()
-		if err != nil {
-			t.Fatalf("error closing node %v", err)
-		}
-	}()
-
-	network, err := NewLibP2PValidatorNetwork(&Peer{host: h}, opts, obs)
-	require.NoError(t, err)
-	require.NotNil(t, network)
-
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	defer ctxCancel()
-	go func() {
-		network.ForwardTransactions(ctx, "receiver_id")
-	}()
-
-	require.Never(t, func() bool { return buf.Len() > 0 }, time.Second, 200*time.Millisecond)
 }

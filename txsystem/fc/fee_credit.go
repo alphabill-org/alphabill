2786a2f86034ded658844c76c1973738d4fa8f2f
Pavel Grigorenko <pgrigorenko@gmail.com>
2024-03-15 17:18:12 +0100

    use common types for Predicate Runner and Executor

--- txsystem/fc/fee_credit.go
+++ txsystem/fc/fee_credit.go
@@ -60,7 +60,7 @@ func NewFeeCreditModule(opts ...Option) (*FeeCredit, error) {
 		if err != nil {
 			return nil, fmt.Errorf("creating predicate executor: %w", err)
 		}
-		m.execPredicate = predicates.PredicateRunner(predEng.Execute, m.state)
+		m.execPredicate = predicates.NewPredicateRunner(predEng.Execute, m.state)
 	}
 	if err := validConfiguration(m); err != nil {
 		return nil, fmt.Errorf("invalid fee credit module configuration: %w", err)
--- txsystem/money/money_tx_system_opts.go
+++ txsystem/money/money_tx_system_opts.go
@@ -1,7 +1,6 @@
 package money
 
 import (
-	"context"
 	"crypto"
 	"fmt"
 
@@ -24,12 +23,10 @@ type (
 		trustBase                map[string]abcrypto.Verifier
 		systemDescriptionRecords []*genesis.SystemDescriptionRecord
 		feeCalculator            fc.FeeCalculator
-		exec                     PredicateExecutor
+		exec                     predicates.PredicateExecutor
 	}
 
 	Option func(*Options)
-
-	PredicateExecutor func(ctx context.Context, predicate types.PredicateBytes, args []byte, txo *types.TransactionOrder, env predicates.TxContext) (bool, error)
 )
 
 func defaultOptions() (*Options, error) {
@@ -87,7 +84,7 @@ func WithFeeCalculator(calc fc.FeeCalculator) Option {
 WithPredicateExecutor allows to replace the default predicate executor function.
 Should be used by tests only.
 */
-func WithPredicateExecutor(exec PredicateExecutor) Option {
+func WithPredicateExecutor(exec predicates.PredicateExecutor) Option {
 	return func(g *Options) {
 		if exec != nil {
 			g.exec = exec

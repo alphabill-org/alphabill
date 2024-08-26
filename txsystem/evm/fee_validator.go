a6db36420a37a6aca89debb3586b6c3957269148
Lennart Ploom <lennart.ploom@guardtime.com>
2024-08-26 11:27:43 +0300

    fix evm fee credit balance validation

--- txsystem/evm/fee_validator.go
+++ txsystem/evm/fee_validator.go
@@ -3,7 +3,6 @@ package evm
 import (
 	"fmt"
 
-	"github.com/alphabill-org/alphabill-go-base/predicates/templates"
 	"github.com/alphabill-org/alphabill-go-base/txsystem/fc"
 	"github.com/alphabill-org/alphabill-go-base/types"
 	"github.com/alphabill-org/alphabill/predicates"
@@ -18,32 +17,54 @@ func isFeeCreditTx(tx *types.TransactionOrder) bool {
 
 func checkFeeAccountBalance(state *state.State, execPredicate func(predicate types.PredicateBytes, args []byte, sigBytes []byte, exeCtx predicates.TxContext) error) genericTransactionValidator {
 	return func(ctx *TxValidationContext) error {
-		if isFeeCreditTx(ctx.Tx) {
-			// TODO extract owner proof from fee tx
-			//addr, err := getAddressFromPredicateArg(ctx.Tx.OwnerProof)
-			ownerProof := templates.EmptyArgument()
-			addr, err := getAddressFromPredicateArg(ownerProof)
-			if err != nil {
-				return fmt.Errorf("failed to extract address from public key bytes, %w", err)
-			}
-			u, _ := state.GetUnit(addr.Bytes(), false)
-			if u == nil && ctx.Tx.PayloadType() == fc.PayloadTypeCloseFeeCredit {
-				return fmt.Errorf("no fee credit info found for unit %X", ctx.Tx.UnitID())
-			}
-			if u == nil && ctx.Tx.PayloadType() == fc.PayloadTypeAddFeeCredit {
-				// account creation
-				return nil
-			}
-			// owner proof verifies correctly
-			sigBytes, err := ctx.Tx.PayloadBytes()
-			if err != nil {
-				return fmt.Errorf("failed to get signature bytes from the transaction: %w", err)
-			}
-			if err = execPredicate(u.Owner(), ownerProof, sigBytes, ctx); err != nil {
-				return fmt.Errorf("invalid owner proof: %w [txOwnerProof=0x%x unitOwnerCondition=0x%x]",
-					err, ownerProof, u.Owner())
-			}
+		if !isFeeCreditTx(ctx.Tx) {
+			return nil
+		}
+		ownerProof, err := getOwnerProof(ctx.Tx)
+		if err != nil {
+			return fmt.Errorf("failed to parse owner proof: %w", err)
+		}
+		addr, err := getAddressFromPredicateArg(ownerProof)
+		if err != nil {
+			return fmt.Errorf("failed to extract address from public key bytes, %w", err)
+		}
+		u, _ := state.GetUnit(addr.Bytes(), false)
+		if u == nil && ctx.Tx.PayloadType() == fc.PayloadTypeCloseFeeCredit {
+			return fmt.Errorf("no fee credit info found for unit %X", ctx.Tx.UnitID())
+		}
+		if u == nil && ctx.Tx.PayloadType() == fc.PayloadTypeAddFeeCredit {
+			// account creation
+			return nil
+		}
+		// owner proof verifies correctly
+		sigBytes, err := ctx.Tx.PayloadBytes()
+		if err != nil {
+			return fmt.Errorf("failed to get signature bytes from the transaction: %w", err)
+		}
+		ownerPredicate := u.Owner()
+		if err = execPredicate(ownerPredicate, ownerProof, sigBytes, ctx); err != nil {
+			return fmt.Errorf("invalid owner proof: %w [authProof.OwnerProof=0x%x unit.Owner=0x%x]", err, ownerProof, ownerPredicate)
 		}
 		return nil
 	}
+}
+
+func getOwnerProof(tx *types.TransactionOrder) ([]byte, error) {
+	var ownerProof []byte
+	if tx.PayloadType() == fc.PayloadTypeAddFeeCredit {
+		var authProof *fc.AddFeeCreditAuthProof
+		if err := types.Cbor.Unmarshal(tx.AuthProof, &authProof); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal %s auth proof: %w", tx.PayloadType(), err)
+		}
+		ownerProof = authProof.OwnerProof
+	} else if tx.PayloadType() == fc.PayloadTypeCloseFeeCredit {
+		var authProof *fc.CloseFeeCreditAuthProof
+		if err := types.Cbor.Unmarshal(tx.AuthProof, &authProof); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal %s auth proof: %w", tx.PayloadType(), err)
+		}
+		ownerProof = authProof.OwnerProof
+	} else {
+		return nil, fmt.Errorf("unsupported transaction type: %s", tx.PayloadType())
+	}
+	return ownerProof, nil
 }
